cf76d37a00329e706c4fe4d7501b5bbdfba41286
shafferm <michael.shaffer@ucdenver.edu>
2019-06-11 17:25:59 -0600

    Add viral to script

--- scripts/annotate_genes.py
+++ scripts/annotate_genes.py
@@ -8,10 +8,11 @@ from checkMetab.annotate_scaffolds import main
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-i', '--input_fasta', help="fasta file with scaffolds, should be nucleotides", required=True)
-    parser.add_argument('--kegg_loc', help='mmseqs2 database file from kegg .pep file', required=True)
-    parser.add_argument('--uniref_loc', help='mmseqs2 database file from uniref .faa', required=True)
-    parser.add_argument('--pfam_loc', help='mmseqs2 database file from pfam .hmm', required=True)
-    parser.add_argument('--dbcan_loc', help='hmm file for dbcan, already processed with hmmpress', required=True)
+    parser.add_argument('--kegg_loc', help='mmseqs2 database file from kegg .pep file')
+    parser.add_argument('--uniref_loc', help='mmseqs2 database file from uniref .faa')
+    parser.add_argument('--pfam_loc', help='mmseqs2 database file from pfam .hmm')
+    parser.add_argument('--dbcan_loc', help='hmm file for dbcan, already processed with hmmpress')
+    parser.add_argument('--viral_loc', help='mmseqs2 database file from ref seq viral gene collection')
     parser.add_argument('-o', '--output_dir', help="output directory")
     parser.add_argument('--min_contig_size', type=int, default=5000,
                         help='minimum contig size to be used for gene prediction')
@@ -29,6 +30,7 @@ if __name__ == '__main__':
     uniref_loc = args.uniref_loc
     pfam_loc = args.pfam_loc
     dbcan_loc = args.dbcan_loc
+    viral_loc = args.viral_loc
     output = args.output_dir
     min_contig_size = args.min_contig_size
     min_bitscore = args.min_bitscore
@@ -37,5 +39,5 @@ if __name__ == '__main__':
     threads = args.threads
     verbose = args.verbose
 
-    main(fasta_loc, kegg_loc, uniref_loc, pfam_loc, dbcan_loc, output, min_contig_size, min_bitscore, strict_bitscore,
+    main(fasta_loc, kegg_loc, uniref_loc, pfam_loc, dbcan_loc, viral_loc, output, min_contig_size, min_bitscore, strict_bitscore,
          keep_tmp, threads, verbose)
