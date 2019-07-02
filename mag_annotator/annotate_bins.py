8c74944d7ed8b17cd23343c63d9846449de737d7
shafferm <michael.shaffer@ucdenver.edu>
2019-07-02 13:26:11 -0600

    Add index for get peptidase description

--- mag_annotator/annotate_bins.py
+++ mag_annotator/annotate_bins.py
@@ -166,7 +166,7 @@ def get_peptidase_description(peptidase_hits, header_dict):
         peptidase_family.append(re.search(r'#\w*.#', header).group()[1:-1])
         peptidase_descirption.append(header)
     new_df = pd.DataFrame([peptidase_list, peptidase_family, peptidase_descirption],
-                          index=['peptidase_id', 'peptidase_family', 'peptidase_hit'])
+                          index=['peptidase_id', 'peptidase_family', 'peptidase_hit'], columns=peptidase_hits.index)
     return pd.concat([new_df.transpose(), peptidase_hits.drop('peptidase_hit', axis=1)], axis=1)
 
 
