2fbec9d3d810fe589cdb4238802950b02bf315de
shafferm <michael.shaffer@ucdenver.edu>
2019-07-27 12:33:55 -0600

    In dbcan search fix the GT2s e.g. GT2_Glyco_tranf_2_3 to GT2 for naming

--- mag_annotator/annotate_bins.py
+++ mag_annotator/annotate_bins.py
@@ -228,13 +228,14 @@ def run_hmmscan_dbcan(genes_faa, dbcan_loc, output_loc, db_handler=None, verbose
 
     dbcan_dict = dict()
     if db_handler is not None:
-        dbcan_descriptions = db_handler.get_descriptions(set([i[:-4] for i in dbcan_res[dbcan_res.significant].tid]),
+        dbcan_descriptions = db_handler.get_descriptions(set([i[:-4].split('_')[0] for i in
+                                                              dbcan_res[dbcan_res.significant].tid]),
                                                          'dbcan_description')
     for gene, frame in dbcan_res[dbcan_res.significant].groupby('qid'):
         if db_handler is None:
             dbcan_dict[gene] = '; '.join([i[:-4] for i in frame.tid])
         else:
-            dbcan_dict[gene] = '; '.join(['%s [%s]' % (dbcan_descriptions[ascession[:-4]], ascession[:-4])
+            dbcan_dict[gene] = '; '.join(['%s [%s]' % (dbcan_descriptions[ascession[:-4].split('_')[0]], ascession[:-4])
                                           for ascession in frame.tid])
     return pd.Series(dbcan_dict, name='cazy_hits')
 
