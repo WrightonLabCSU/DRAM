9545c080718745e991f7e34eb20ae4bd488da351
shafferm <michael.shaffer@ucdenver.edu>
2019-08-19 14:55:04 -0600

    Fix spacing in setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ __author__ = 'shafferm'
 __version__ = version
 
 setup(
-      name="DRAM",
-      version=__version__,
-      scripts=['scripts/DRAM.py'],
-      packages=find_packages(),
-      package_data={'mag_annotator': ['CONFIG']},
-      install_requires=['scikit-bio', 'pandas', 'altair', 'sqlalchemy'],
-      description="Annotate contigs/bins from metagenomic assemblies and create predicted metabolisms",
-      author="Michael Shaffer",
-      author_email='michael.t.shaffer@colostate.edu',
-      url="https://github.com/shafferm/DRAM/",
-      download_url="https://github.com/shafferm/DRAM/tarball/%s" % __version__
+    name="DRAM",
+    version=__version__,
+    scripts=['scripts/DRAM.py'],
+    packages=find_packages(),
+    package_data={'mag_annotator': ['CONFIG']},
+    install_requires=['scikit-bio', 'pandas', 'altair', 'sqlalchemy'],
+    description="Annotate contigs/bins from metagenomic assemblies and create predicted metabolisms",
+    author="Michael Shaffer",
+    author_email='michael.t.shaffer@colostate.edu',
+    url="https://github.com/shafferm/DRAM/",
+    download_url="https://github.com/shafferm/DRAM/tarball/%s" % __version__
 )
