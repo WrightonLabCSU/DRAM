9f331705b62a8ca1069ecf220c43399a89868aeb
shafferm <michael.shaffer@ucdenver.edu>
2019-09-20 15:41:20 -0600

    Merge new distillate and liquor, rotate module coverage heatmap

--- scripts/DRAM.py
+++ scripts/DRAM.py
@@ -61,6 +61,8 @@ if __name__ == '__main__':
     set_db_locs_parser.add_argument('--dbcan_db_loc', default=None,
                                     help='hmm file for dbcan, already processed with hmmpress')
     set_db_locs_parser.add_argument('--dbcan_fam_activities', default=None, help='CAZY family activities file')
+    set_db_locs_parser.add_argument('--vogdb_db_loc', default=None,
+                                    help='hmm file for vogdb, already processed with hmmpress')
     set_db_locs_parser.add_argument('--viral_db_loc', default=None,
                                     help='mmseqs2 database file from ref seq viral gene collection')
     set_db_locs_parser.add_argument('--peptidase_db_loc', default=None,
