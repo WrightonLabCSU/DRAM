e556a979b0776a9f138c0de451a1030ab0ef9d88
shafferm <michael.shaffer@ucdenver.edu>
2020-02-13 13:04:21 -0700

    Don't set default to tuple when using action='append'

--- scripts/DRAM.py
+++ scripts/DRAM.py
@@ -121,8 +121,8 @@ if __name__ == '__main__':
                                       "must match nubmer of custom_db_name's")
     annotate_parser.add_argument('--gtdb_taxonomy', action='append',
                                  help='Summary file from gtdbtk taxonomy assignment from bins, can be used multiple'
-                                      'times', default=())
-    annotate_parser.add_argument('--checkm_quality', action='append', default=(),
+                                      'times')
+    annotate_parser.add_argument('--checkm_quality', action='append',
                                  help='Summary of of checkM quality assessment from bins, can be used multiple times')
     annotate_parser.add_argument('--genes_called', action='store_true', default=False,
                                  help='if you are passing a file of amino acid sequences')
