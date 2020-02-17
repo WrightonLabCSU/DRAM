898582fdbbdfa927f73aa3f494ca233447b2eb19
shafferm <michael.shaffer@ucdenver.edu>
2020-02-16 18:30:13 -0700

    For getting J flag check vogdb_categories is not NA

--- mag_annotator/annotate_vgfs.py
+++ mag_annotator/annotate_vgfs.py
@@ -197,7 +197,8 @@ def get_metabolic_flags(annotations, metabolic_genes, amgs, verified_amgs, scaff
     flag_dict = dict()
     metabolic_genes = set(metabolic_genes)
     for scaffold, scaffold_annotations in annotations.groupby('scaffold'):
-        perc_xh = sum(['Xh' in i for i in scaffold_annotations['vogdb_categories']]) / scaffold_annotations.shape[0]
+        perc_xh = sum(['Xh' in i if not pd.isna(i) else False for i in scaffold_annotations['vogdb_categories']]) \
+                  / scaffold_annotations.shape[0]
         is_j = perc_xh >= 0.18
         for gene, row in scaffold_annotations.iterrows():
             # set up
