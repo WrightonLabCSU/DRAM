076fcc4015f54e5593b7bef8cab702d872aa05a6
rmflynn <rmfofco@gmail.com>
2023-01-07 10:02:42 -0700

    add a config test at last

---
+++ tests/test_config_cleen.py
@@ -0,0 +1,51 @@
+"""
+One test to stop so much pain, did you accidentally commit a test config?
+This tests that the config is clean. It will fail when you are testing a working copy but not when you push to github.
+
+"""
+
+import pytest
+import logging
+from os import path
+from mag_annotator.database_handler import DatabaseHandler
+from mag_annotator.utils import setup_logger
+
+
+@pytest.fixture()
+def logger(tmpdir):
+    logger = logging.getLogger('test_log')
+    setup_logger(logger)
+    return logger
+
+
+def db_w_entries(logger):
+    empty_config = {
+        "search_databases": {
+        "kegg": None,
+        "kofam_hmm": None,
+        "kofam_ko_list": None,
+        "uniref": None,
+        "pfam": None,
+        "dbcan": None,
+        "viral": None,
+        "peptidase": None,
+        "vogdb": None
+      },
+      "custom_dbs": None,
+      "database_descriptions": {
+        "pfam_hmm_dat": None,
+        "dbcan_fam_activities": None,
+        "vog_annotations": None
+      },
+      "dram_sheets": {
+        "genome_summary_form": None,
+        "module_step_form": None,
+        "etc_module_database": None,
+        "function_heatmap_form": None,
+        "amg_database": None
+      },
+      "description_db": None,
+      "dram_version": None
+    }
+    db_handler = DatabaseHandler(logger, path.join('tests', 'data', 'test_CONFIG'))
+    assert db_handler.config == empty_config
