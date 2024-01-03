7bdf973f3f306a9fa42ee54acff2c12080f65a37
Reed Woyda <reedwoyda@gmail.com>
2024-01-03 15:01:25 -0700

    Changing the database location from a val to a path in hmmsearch.nf

--- assets/parse_hmmsearch.py
+++ assets/parse_hmmsearch.py
@@ -6,11 +6,10 @@ HMMSCAN_ALL_COLUMNS = ['query_id', 'query_ascession', 'query_length', 'target_id
 HMMSCAN_COLUMN_TYPES = [str, str, int, str, str, int, float, float, float, int, int, float, float, float, float, int, int, int, int, int, int, float, str]
 
 def parse_hmmsearch_domtblout(file):
-    df_lines = list()
+    df_lines = []
     for line in open(file):
         if not line.startswith('#'):
             line = line.split()
-            line = line[:22] + [' '.join(line[22:])]
             df_lines.append(line)
     hmmsearch_frame = pd.DataFrame(df_lines, columns=HMMSCAN_ALL_COLUMNS)
     for i, column in enumerate(hmmsearch_frame.columns):
@@ -21,24 +20,7 @@ if __name__ == '__main__':
     input_file = sys.argv[1]
     output_file = sys.argv[2]
     result = parse_hmmsearch_domtblout(input_file)
-
-    # Debugging print statements
-    print("Parsed DataFrame:")
-    print(result)
-
-    if result.empty:
-        print("Parsed DataFrame is empty. No further processing.")
-    else:
-        # Example: Call the kofam_hmmscan_formater function
-        # You might need to adapt this based on your actual usage
-        kegg_result = kofam_hmmscan_formater(result, hmm_info_path="/path/to/hmm_info.tsv")
-        print("Result of kofam_hmmscan_formater:")
-        print(kegg_result)
-
-        # Save the result DataFrame to CSV
-        kegg_result.to_csv(output_file, index=False)
-
-
+    result.to_csv(output_file, index=False)
 
 
 
