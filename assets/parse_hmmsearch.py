bd8a0f03d620b469a2103e656864c60d623486b9
Reed Woyda <reedwoyda@gmail.com>
2024-01-03 14:51:36 -0700

    Changing the database location from a val to a path in hmmsearch.nf

--- assets/parse_hmmsearch.py
+++ assets/parse_hmmsearch.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python
 import sys
 import pandas as pd
+import re
 
 HMMSCAN_ALL_COLUMNS = ['query_id', 'query_ascession', 'query_length', 'target_id', 'target_ascession', 'target_length', 'full_evalue', 'full_score', 'full_bias', 'domain_number', 'domain_count', 'domain_cevalue', 'domain_ievalue', 'domain_score', 'domain_bias', 'target_start', 'target_end', 'alignment_start', 'alignment_end', 'query_start', 'query_end', 'accuracy', 'description']
 HMMSCAN_COLUMN_TYPES = [str, str, int, str, str, int, float, float, float, int, int, float, float, float, float, int, int, int, int, int, int, float, str]
@@ -9,11 +9,9 @@ def parse_hmmsearch_domtblout(file):
     df_lines = list()
     for line in open(file):
         if not line.startswith('#'):
-            fields = line.split()
-            if len(fields) >= 22:  # Check if the line has the expected number of fields
-                line = fields[:22] + [' '.join(fields[22:])]
-                df_lines.append(line)
-    
+            line = re.split(r'\s+', line.strip())
+            line = line[:22] + [' '.join(line[22:])]
+            df_lines.append(line)
     hmmsearch_frame = pd.DataFrame(df_lines, columns=HMMSCAN_ALL_COLUMNS)
     for i, column in enumerate(hmmsearch_frame.columns):
         hmmsearch_frame[column] = hmmsearch_frame[column].astype(HMMSCAN_COLUMN_TYPES[i])
