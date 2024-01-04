ede75e1f1ed31d6a8cf7b52177e85ae275c290e0
Reed Woyda <reedwoyda@gmail.com>
2024-01-04 14:03:21 -0700

    Added modifications for working with dbcan

--- assets/dbcan_hmm_formatter.py
+++ assets/dbcan_hmm_formatter.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import argparse
 
+def process_additional_fields(line):
+    # Append data from additional fields to the second column
+    return '\t'.join([line[1], '\t'.join(line[2:])])
+
 def dbcan_hmmscan_formater(hits, ch_dbcan_fam, ch_dbcan_subfam):
     try:
         # Check if 'query_id' column is present
@@ -27,20 +31,15 @@ def dbcan_hmmscan_formater(hits, ch_dbcan_fam, ch_dbcan_subfam):
         fam_mapping = pd.read_csv(ch_dbcan_fam, sep='\t', index_col=0, comment='#', header=None, names=['family-activities'], error_bad_lines=False)
         hits = hits.join(fam_mapping, on='family')
 
-        # Handle lines with additional columns in family activities file
-        fam_mapping[1] = fam_mapping.groupby(fam_mapping.index)['family-activities'].transform(lambda x: "; ".join(x.astype(str)))
-        fam_mapping = fam_mapping.drop_duplicates()
-
-        hits = hits.join(fam_mapping, on='family')
-
         # Extract 'subfam-EC' and 'subfam-GenBank' based on 'target_id'
-        subfam_mapping = pd.read_csv(ch_dbcan_subfam, sep='\t', header=None, names=['subfam-EC', 'subfam-GenBank'])
-
-        # Concatenate values when there are multiple matches
-        subfam_mapping = subfam_mapping.groupby(0).agg(lambda x: "; ".join(x.astype(str))).reset_index()
+        subfam_mapping = pd.read_csv(ch_dbcan_subfam, sep='\t', header=None, names=['subfam-GenBank', 'subfam-EC'])
         hits = hits.join(subfam_mapping.set_index(0), on='subfamily')
 
-        # Concatenate values when there are multiple matches for additional columns
+        # Concatenate values when there are multiple matches
+        hits['subfam-EC'] = hits.groupby('query_id')['subfam-EC'].transform(lambda x: "; ".join(x))
+        hits['subfam-GenBank'] = hits.groupby('query_id')['subfam-GenBank'].transform(lambda x: "; ".join(x))
+
+        # Handle lines with additional columns
         additional_columns = [col for col in hits.columns[23:] if hits[col].notna().any()]
         hits['family-activities'] = hits.apply(lambda row: "; ".join(filter(None, [row['family-activities']] + row[additional_columns].astype(str))), axis=1)
 
@@ -58,8 +57,8 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    # Read CSV file with comma as the delimiter and handle lines with too many fields
-    hits_df = pd.read_csv(args.hits_csv, delimiter=',', on_bad_lines='skip')
+    # Read CSV file with comma as the delimiter
+    hits_df = pd.read_csv(args.hits_csv, delimiter=',', error_bad_lines=False)
 
     # Format hits
     formatted_hits = dbcan_hmmscan_formater(hits_df, args.fam, args.subfam)
