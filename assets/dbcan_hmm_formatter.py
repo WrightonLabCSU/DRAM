f862f99a84a4f56f6ae8002e26ae69eadc320f62
Reed Woyda <reedwoyda@gmail.com>
2024-01-04 12:58:32 -0700

    Added modifications for working with dbcan

--- assets/dbcan_hmm_formatter.py
+++ assets/dbcan_hmm_formatter.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import argparse
-import re
-from functools import partial
 
 def get_sig_row(row, evalue_lim):
     return row['full_evalue'] < evalue_lim
@@ -10,7 +8,7 @@ def find_best_dbcan_hit(group):
     best_hit = group[group['full_evalue'] == group['full_evalue'].min()]
     return best_hit['target_id'].values[0]
 
-def dbcan_hmmscan_formater(hits, ch_dbcan_fam, ch_dbcan_subfam):
+def dbcan_hmmscan_formatter(hits, ch_dbcan_fam, ch_dbcan_subfam):
     # Sort hits within each group based on 'full_evalue'
     hits['rank'] = hits.groupby('query_id')['full_evalue'].rank()
 
@@ -28,7 +26,7 @@ def dbcan_hmmscan_formater(hits, ch_dbcan_fam, ch_dbcan_subfam):
     hits['dbcan-best-hit'] = hits.groupby('query_id')['target_id'].transform(lambda x: x.str[:-4].unique().min())
 
     # Extract 'family-activities' based on 'target_id'
-    fam_mapping = pd.read_csv(ch_dbcan_fam, sep='\t', index_col=0, header=None, names=['family-activities'])
+    fam_mapping = pd.read_csv(ch_dbcan_fam, sep='\t', index_col=0, comment='#', header=None, names=['family-activities'], error_bad_lines=False)
     hits = hits.join(fam_mapping, on='family')
 
     # Extract 'subfam-EC' and 'subfam-GenBank' based on 'target_id'
@@ -50,5 +48,5 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    formatted_hits = dbcan_hmmscan_formater(pd.read_csv(args.hits_csv), args.fam, args.subfam)
+    formatted_hits = dbcan_hmmscan_formatter(pd.read_csv(args.hits_csv), args.fam, args.subfam)
     formatted_hits.to_csv(args.output, index=False)
