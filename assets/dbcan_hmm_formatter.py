8a7be258b5debedddc39d59abf5bc11b2e15b34a
Reed Woyda <reedwoyda@gmail.com>
2024-01-04 14:09:36 -0700

    Added modifications for working with dbcan

--- assets/dbcan_hmm_formatter.py
+++ assets/dbcan_hmm_formatter.py
@@ -30,6 +30,7 @@ def dbcan_hmmscan_formater(hits, ch_dbcan_fam, ch_dbcan_subfam):
         # Extract 'family-activities' based on 'target_id'
         fam_mapping = pd.read_csv(ch_dbcan_fam, sep='\t', index_col=0, comment='#', header=None, names=['family-activities'], error_bad_lines=False, warn_bad_lines=False)
         fam_mapping.index = fam_mapping.index.map(str)  # Convert index to string to avoid issues
+        hits['family'] = hits['family'].astype(str)  # Convert 'family' column to string
         hits = hits.join(fam_mapping, on='family')
 
         # Extract 'subfam-EC' and 'subfam-GenBank' based on 'target_id'
