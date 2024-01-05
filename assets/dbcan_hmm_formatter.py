0076333c06d6ce0328325418161aeec030acb8ae
Reed Woyda <reedwoyda@gmail.com>
2024-01-05 08:27:05 -0700

    Added modifications for working with dbcan

--- assets/dbcan_hmm_formatter.py
+++ assets/dbcan_hmm_formatter.py
@@ -1,6 +1,20 @@
 import pandas as pd
 import argparse
 
+def extract_subfamily_description(row, fam_data):
+    target_id = row['target_id'].replace('.hmm', '')  # Remove the ".hmm" extension
+    if target_id in fam_data.index:
+        return fam_data.loc[target_id, 'Description']
+    else:
+        return None
+
+def extract_genbank_ec(row, subfam_data):
+    target_id = row['target_id'].replace('.hmm', '')  # Remove the ".hmm" extension
+    if target_id in subfam_data.index:
+        return subfam_data.loc[target_id, 'Genbank'], subfam_data.loc[target_id, 'EC']
+    else:
+        return None, None
+
 def get_sig_row(row):
     return row['full_evalue'] < 1e-5
 
@@ -10,33 +24,24 @@ def bitScore_per_row(row):
 def rank_per_row(row):
     return row['score_rank'] if 'score_rank' in row and row['full_score'] > row['score_rank'] else row['full_score']
 
-def process_dbcan_hits(hits, fam, subfam):
-    # Create subfamily column
-    hits['subfamily'] = hits['target_id'].str.rstrip('.hmm').map(fam.set_index('AA')['Description'])
-    
-    # Create subfam-GenBank and subfam-EC columns
-    hits['subfam-GenBank'] = hits['target_id'].str.rstrip('.hmm').map(subfam.set_index('AA1_1')['GenBank'])
-    hits['subfam-EC'] = hits['target_id'].str.rstrip('.hmm').map(subfam.set_index('AA1_1')['EC'])
-    
-    return hits[['query_id', 'target_id', 'score_rank', 'bitScore', 'subfamily', 'subfam-GenBank', 'subfam-EC']]
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Format KEGG HMM search results.")
     parser.add_argument("--hits_csv", type=str, help="Path to the HMM search results CSV file.")
-    parser.add_argument("--fam", type=str, help="Path to the ch_dbcan_fam file.")
-    parser.add_argument("--subfam", type=str, help="Path to the ch_dbcan_subfam file.")
+    parser.add_argument("--fam", type=str, help="Path to the CAZyDB fam-activities file.")
+    parser.add_argument("--subfam", type=str, help="Path to the CAZyDB fam.subfam.ec file.")
     parser.add_argument("--output", type=str, help="Path to the formatted output file.")
 
     args = parser.parse_args()
 
+    fam_data = pd.read_csv(args.fam, sep="\t", comment='#', header=None, names=['AA', 'Description'], usecols=[0, 1], index_col=0)
+    subfam_data = pd.read_csv(args.subfam, sep="\t", header=None, names=['Subfamily', 'Genbank', 'EC'], usecols=[0, 1, 2], index_col=0)
     hits = pd.read_csv(args.hits_csv)
-    ch_dbcan_fam = pd.read_csv(args.fam, sep="\t", comment='#', header=None, names=['AA', 'Description'])
 
-    # Assuming ch_dbcan_subfam has columns 'AA1_1', 'GenBank', 'EC'
-    ch_dbcan_subfam = pd.read_csv(args.subfam, sep="\t", header=None, names=['AA1_1', 'GenBank', 'EC'])
+    hits['subfamily'] = hits.apply(lambda row: extract_subfamily_description(row, fam_data), axis=1)
+    hits['subfam-GenBank'], hits['subfam-EC'] = zip(*hits.apply(lambda row: extract_genbank_ec(row, subfam_data), axis=1))
+    hits['bitScore'] = hits.apply(bitScore_per_row, axis=1)
+    hits['score_rank'] = hits.apply(rank_per_row, axis=1)
+    hits.dropna(subset=['score_rank'], inplace=True)
+    hits = hits[['query_id', 'target_id', 'score_rank', 'bitScore', 'subfamily', 'subfam-GenBank', 'subfam-EC']]
 
-    # Perform the processing
-    formatted_hits = process_dbcan_hits(hits, ch_dbcan_fam, ch_dbcan_subfam)
-
-    # Save the formatted hits
-    formatted_hits.to_csv(args.output, sep="\t", index=False)
+    hits.to_csv(args.output, sep="\t", index=False)
