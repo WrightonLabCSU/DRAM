aa6448a883032782ae1334761c6841bbae65767d
Reed Woyda <reedwoyda@gmail.com>
2024-01-05 08:44:58 -0700

    Added modifications for working with dbcan

--- assets/dbcan_hmm_formatter.py
+++ assets/dbcan_hmm_formatter.py
@@ -1,59 +1,58 @@
 import pandas as pd
 import argparse
 
-def score_rank_per_row(row):
-    return row['full_score'] if row['full_score'] > row['score_rank'] else row['score_rank']
+def get_sig_row(row):
+    return row['full_evalue'] < 1e-5
 
 def bitScore_per_row(row):
     return row['full_score'] / row['domain_number']
 
+def rank_per_row(row):
+    return row['score_rank'] if 'score_rank' in row and row['full_score'] > row['score_rank'] else row['full_score']
+
+def generate_subfamily(row, ch_dbcan_subfam):
+    target_id = row['target_id']
+    matching_rows = ch_dbcan_subfam[ch_dbcan_subfam['target_id'] == target_id]
+    if not matching_rows.empty:
+        return "; ".join(matching_rows['subfamily'])
+    else:
+        return ""
+
+def generate_subfam_GenBank(row, ch_dbcan_subfam):
+    target_id = row['target_id']
+    matching_rows = ch_dbcan_subfam[ch_dbcan_subfam['target_id'] == target_id]
+    if not matching_rows.empty:
+        return "; ".join(matching_rows['subfam-GenBank'])
+    else:
+        return ""
+
+def generate_subfam_EC(row, ch_dbcan_subfam):
+    target_id = row['target_id']
+    matching_rows = ch_dbcan_subfam[ch_dbcan_subfam['target_id'] == target_id]
+    if not matching_rows.empty:
+        return "; ".join(matching_rows['subfam-EC'])
+    else:
+        return ""
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Format DBCAN HMM search results.")
+    parser = argparse.ArgumentParser(description="Format HMM search results.")
     parser.add_argument("--hits_csv", type=str, help="Path to the HMM search results CSV file.")
-    parser.add_argument("--fam", type=str, help="Path to ch_dbcan_fam file.")
-    parser.add_argument("--subfam", type=str, help="Path to ch_dbcan_subfam file.")
+    parser.add_argument("--fam", type=str, help="Path to the fam file.")
+    parser.add_argument("--subfam", type=str, help="Path to the subfam file.")
     parser.add_argument("--output", type=str, help="Path to the formatted output file.")
 
     args = parser.parse_args()
 
-    hits_df = pd.read_csv(args.hits_csv, sep=",")  # Assuming comma as the delimiter
+    hits_df = pd.read_csv(args.hits_csv)
+    ch_dbcan_subfam = pd.read_csv(args.subfam, sep="\t", comment='#', header=None, names=['target_id', 'subfamily', 'subfam-GenBank', 'subfam-EC'])
 
-    # Calculate 'score_rank' and 'bitScore' for each row
-    hits_df['score_rank'] = hits_df.apply(score_rank_per_row, axis=1)
     hits_df['bitScore'] = hits_df.apply(bitScore_per_row, axis=1)
+    hits_df['score_rank'] = hits_df.apply(rank_per_row, axis=1)
+    hits_df.dropna(subset=['score_rank'], inplace=True)
 
-    # Debugging: Print column names and contents of hits_df after calculations
-    print("Column names after calculations:", hits_df.columns)
-    print("Contents of hits_df after calculations:")
-    print(hits_df.head())
+    hits_df['subfamily'] = hits_df.apply(lambda row: generate_subfamily(row, ch_dbcan_subfam), axis=1)
+    hits_df['subfam-GenBank'] = hits_df.apply(lambda row: generate_subfam_GenBank(row, ch_dbcan_subfam), axis=1)
+    hits_df['subfam-EC'] = hits_df.apply(lambda row: generate_subfam_EC(row, ch_dbcan_subfam), axis=1)
 
-
-    # Read ch_dbcan_fam file
-    ch_dbcan_fam = read_ch_dbcan_fam(args.fam)
-
-    # Read ch_dbcan_subfam file
-    ch_dbcan_subfam = pd.read_csv(args.subfam, sep="\t", header=None, names=['AA', 'GenBank', 'EC'])
-
-    # Remove ".hmm" extension from target_id in hits_df
-    hits_df['target_id'] = hits_df['target_id'].apply(remove_extension)
-
-    # Merge hits_df with ch_dbcan_fam to get subfamily
-    hits_df = pd.merge(hits_df, ch_dbcan_fam, left_on='target_id', right_on='AA', how='left')
-
-    # Merge hits_df with ch_dbcan_subfam to get GenBank and EC
-    merged_df = pd.merge(hits_df, ch_dbcan_subfam, on='AA', how='left')
-
-    # Group by query_id and aggregate multiple GenBank and EC values
-    grouped_df = merged_df.groupby('query_id').agg({
-        'target_id': 'first',
-        'bitScore': 'first',
-        'subfamily': 'first',
-        'GenBank': lambda x: "; ".join(x.dropna()),  # Concatenate GenBank values
-        'EC': lambda x: "; ".join(x.dropna())  # Concatenate EC values
-    }).reset_index()
-
-    # Save the formatted hits to the output file
-    grouped_df.to_csv(args.output, sep="\t", index=False)
-
-if __name__ == "__main__":
-    main()
+    hits_df = hits_df[['query_id', 'target_id', 'score_rank', 'bitScore', 'subfamily', 'subfam-GenBank', 'subfam-EC']]
+    hits_df.to_csv(args.output, sep="\t", index=False)
