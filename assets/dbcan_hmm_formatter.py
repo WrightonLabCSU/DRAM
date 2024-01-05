7ba319ad77adce1dcad872eb64e7eca5118a5ff2
Reed Woyda <reedwoyda@gmail.com>
2024-01-05 10:37:31 -0700

    Hardcoded supporting python scripts for now

--- assets/dbcan_hmm_formatter.py
+++ assets/dbcan_hmm_formatter.py
@@ -4,7 +4,7 @@ import argparse
 def get_sig_row(row):
     return row['full_evalue'] < 1e-5
 
-def bitScore_per_row(row):
+def bit_score_per_row(row):
     return row['full_score'] / row['domain_number']
 
 def rank_per_row(row):
@@ -15,19 +15,20 @@ def generate_subfamily(row, ch_dbcan_subfam, ch_dbcan_fam):
     matching_rows_subfam = ch_dbcan_subfam[ch_dbcan_subfam['target_id'].str.contains(target_id)]
     matching_rows_fam = ch_dbcan_fam[ch_dbcan_fam['target_id'].str.contains(target_id)]
     
-    if not matching_rows_subfam.empty:
-        return matching_rows_subfam.iloc[0]['subfamily']
-    elif not matching_rows_fam.empty:
-        return matching_rows_fam.iloc[0]['subfamily']
+    subfamily = matching_rows_subfam.iloc[0]['subfamily'] if not matching_rows_subfam.empty else ""
+    
+    if subfamily == "":
+        fam_subfamily = matching_rows_fam.iloc[0]['subfamily'] if not matching_rows_fam.empty else ""
+        return fam_subfamily
     else:
-        return ""
+        return subfamily
 
-def generate_subfam_GenBank(row, ch_dbcan_subfam):
+def generate_subfam_genbank(row, ch_dbcan_subfam):
     target_id = row['target_id']
     matching_rows = ch_dbcan_subfam[ch_dbcan_subfam['target_id'].str.contains(target_id)]
     return "; ".join(matching_rows['subfam-GenBank']) if not matching_rows.empty else ""
 
-def generate_subfam_EC(row, ch_dbcan_subfam):
+def generate_subfam_ec(row, ch_dbcan_subfam):
     target_id = row['target_id']
     matching_rows = ch_dbcan_subfam[ch_dbcan_subfam['target_id'].str.contains(target_id)]
     return "; ".join(map(str, matching_rows['subfam-EC'].dropna())) if not matching_rows.empty else ""
@@ -43,8 +44,11 @@ def main():
 
     # Read HMM search results CSV file and subfam file
     hits_df = pd.read_csv(args.hits_csv)
-    ch_dbcan_subfam = pd.read_csv(args.subfam, sep="\t", comment='#', header=None, names=['target_id', 'subfamily', 'subfam-GenBank', 'subfam-EC'], engine='python')
-    ch_dbcan_fam = pd.read_csv(args.fam, comment='#', header=None, names=['target_id', 'subfamily'], engine='python', error_bad_lines=False, delimiter='\t', usecols=[0, 1], quoting=3)
+    ch_dbcan_subfam = pd.read_csv(args.subfam, sep="\t", comment='#', header=None,
+                                  names=['target_id', 'subfamily', 'subfam-GenBank', 'subfam-EC'], engine='python')
+    ch_dbcan_fam = pd.read_csv(args.fam, comment='#', header=None,
+                               names=['target_id', 'subfamily'], engine='python', error_bad_lines=False,
+                               delimiter='\t', usecols=[0, 1], quoting=3)
 
     # Remove the '.hmm' extension from 'target_id' in hits_df
     hits_df['target_id'] = hits_df['target_id'].str.replace(r'.hmm', '', regex=True)
@@ -69,30 +73,30 @@ def main():
     print(ch_dbcan_subfam.head())
 
     # Add new columns to hits_df
-    hits_df['bitScore'] = hits_df.apply(bitScore_per_row, axis=1)
+    hits_df['bitScore'] = hits_df.apply(bit_score_per_row, axis=1)
     hits_df['score_rank'] = hits_df.apply(rank_per_row, axis=1)
     hits_df.dropna(subset=['score_rank'], inplace=True)
 
     # Filter matching rows between hits_df and ch_dbcan_subfam
     matching_rows = hits_df[hits_df['target_id'].isin(ch_dbcan_subfam['target_id'])]
     print("Matching rows between hits_df and ch_dbcan_subfam:")
-    print(matching_rows[['query_id', 'target_id', 'score_rank', 'bitScore']])
+    print(matching_rows[['target_id', 'bitScore', 'score_rank']].head())
 
-    # Remove duplicates from ch_dbcan_subfam DataFrame
-    ch_dbcan_subfam = ch_dbcan_subfam.drop_duplicates(subset='target_id')
-
-    # Update the mapping in hits_df with correct column assignments
+    # Generate subfamily information
     hits_df['subfamily'] = hits_df.apply(lambda row: generate_subfamily(row, ch_dbcan_subfam, ch_dbcan_fam), axis=1)
-    hits_df['subfam-GenBank'] = hits_df.apply(generate_subfam_GenBank, axis=1, ch_dbcan_subfam=ch_dbcan_subfam)
-    hits_df['subfam-EC'] = hits_df.apply(generate_subfam_EC, axis=1, ch_dbcan_subfam=ch_dbcan_subfam)
 
-    # Print column names and contents of hits_df
-    print("Column names of hits_df:", hits_df.columns)
-    print("Contents of hits_df:")
-    print(hits_df)
+    # Generate subfam-GenBank and subfam-EC information
+    hits_df['subfam-GenBank'] = hits_df.apply(lambda row: generate_subfam_genbank(row, ch_dbcan_subfam), axis=1)
+    hits_df['subfam-EC'] = hits_df.apply(lambda row: generate_subfam_ec(row, ch_dbcan_subfam), axis=1)
 
-    # Save the formatted output to a file
-    hits_df[['query_id', 'target_id', 'score_rank', 'bitScore', 'subfamily', 'subfam-GenBank', 'subfam-EC']].to_csv(args.output, sep="\t", index=False)
+    # Filter significant rows
+    sig_hits_df = hits_df[hits_df.apply(get_sig_row, axis=1)]
+
+    # Sort the significant hits by score rank
+    sig_hits_df = sig_hits_df.sort_values(by='score_rank')
+
+    # Save the formatted output to a new CSV file
+    sig_hits_df.to_csv(args.output, index=False)
 
 if __name__ == "__main__":
     main()
