3094c15b196e2132fef930bf29e8f04ffd5f0031
Reed Woyda <reedwoyda@gmail.com>
2024-01-08 09:45:01 -0700

    Modified kofam_formatter to clean up the kofam_EC column

--- assets/kofam_hmm_formatter.py
+++ assets/kofam_hmm_formatter.py
@@ -3,45 +3,34 @@ import argparse
 import re
 
 def calculate_bit_score(row):
-    """Calculate bit score for each row."""
     return row['full_score'] / row['domain_number']
 
 def calculate_rank(row):
-    """Calculate rank for each row."""
     return row['score_rank'] if 'score_rank' in row and row['full_score'] > row['score_rank'] else row['full_score']
 
 def find_best_dbcan_hit(df):
-    """Find the best hit based on full_evalue."""
     df.sort_values("full_evalue", inplace=True)
     return df.iloc[0]["target_id"]
 
 def mark_best_hit_based_on_rank(df):
-    """Mark the best hit for each unique query_id based on score_rank."""
     best_hit_idx = df["score_rank"].idxmin()
     df.at[best_hit_idx, "best_hit"] = True
     return df
 
-def clean_ec_numbers(ec_entry):
-    """Clean up EC numbers by removing '[EC:' and ']'."""
-    ec_numbers = re.findall(r'\[EC:(.*?)\]', ec_entry)
-    cleaned_ec_numbers = '; '.join(ec_numbers)
-    return cleaned_ec_numbers
-
 def main():
-    # Command-line arguments
     parser = argparse.ArgumentParser(description="Format HMM search results.")
     parser.add_argument("--hits_csv", type=str, help="Path to the HMM search results CSV file.")
     parser.add_argument("--ch_kofam_ko", type=str, help="Path to the ch_kofam_ko file.")
     parser.add_argument("--output", type=str, help="Path to the formatted output file.")
+
     args = parser.parse_args()
 
-    # Load HMM search results CSV file
     print("Loading HMM search results CSV file...")
     hits_df = pd.read_csv(args.hits_csv)
 
-    # Preprocess HMM search results
     print("Processing HMM search results...")
     hits_df['target_id'] = hits_df['target_id'].str.replace(r'.hmm', '', regex=True)
+
     hits_df['bitScore'] = hits_df.apply(calculate_bit_score, axis=1)
     hits_df['score_rank'] = hits_df.apply(calculate_rank, axis=1)
     hits_df.dropna(subset=['score_rank'], inplace=True)
@@ -55,7 +44,6 @@ def main():
     # Mark the best hit for each unique query_id based on score_rank
     hits_df = hits_df.groupby('query_id').apply(mark_best_hit_based_on_rank).reset_index(drop=True)
 
-    # Load ch_kofam_ko file
     print("Loading ch_kofam_ko file...")
     ch_kofam_ko_df = pd.read_csv(args.ch_kofam_ko, sep="\t")
 
@@ -66,8 +54,8 @@ def main():
     merged_df['kofam_definition'] = merged_df['definition'].apply(lambda x: re.sub(r' \[EC:[^\]]*\]', '', str(x)) if pd.notna(x) else '')
     merged_df['kofam_EC'] = merged_df['definition'].apply(lambda x: re.search(r'\[EC:[^\]]*\]', str(x)).group(0) if re.search(r'\[EC:[^\]]*\]', str(x)) else '')
 
-    # Clean up kofam_EC entries by removing '[EC:' and ']'
-    merged_df['kofam_EC'] = merged_df['kofam_EC'].apply(clean_ec_numbers)
+    # Concatenate kofam_EC values with "; "
+    merged_df['kofam_EC'] = merged_df.groupby('query_id')['kofam_EC'].transform(lambda x: '; '.join(x))
 
     # Save the formatted output to CSV
     selected_columns = ['query_id', 'target_id', 'score_rank', 'bitScore', 'kofam_definition', 'kofam_EC']
