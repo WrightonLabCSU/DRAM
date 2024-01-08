8a501b7fe5522481c44cf55177af7d9d2ce2e563
Reed Woyda <reedwoyda@gmail.com>
2024-01-08 15:04:56 -0700

    Modified kofam_formatter to clean up the kofam_EC column

--- assets/combine_annotations.py
+++ assets/combine_annotations.py
@@ -20,7 +20,8 @@ def combine_annotations(annotation_files, output_file):
 
         # Check if the file exists
         if not os.path.exists(file_path):
-            raise ValueError(f"Could not find file: {file_path}")
+            logging.warning(f"Could not find file: {file_path}")
+            continue
 
         # Read each annotation file
         logging.info(f"Processing annotation file: {file_path}")
@@ -29,16 +30,18 @@ def combine_annotations(annotation_files, output_file):
             # Read CSV with custom separator and without specifying names
             annotation_data = pd.read_csv(file_path, sep=',', header=0)
         except FileNotFoundError:
-            raise ValueError(f"Could not find file: {file_path}")
+            logging.warning(f"Could not find file: {file_path}")
+            continue
 
         # Make the script case-insensitive when checking for 'query_id'
         query_id_col = [col for col in annotation_data.columns if col.lower() == 'query_id']
         if not query_id_col:
-            raise ValueError(f"Column 'query_id' not found in the annotation file: {file_path}")
+            logging.warning(f"Column 'query_id' not found in the annotation file: {file_path}")
+            continue
         query_id_col = query_id_col[0]
 
         # Merge the current annotation_data with combined_data based on 'query_id'
-        combined_data = pd.merge(combined_data, annotation_data, on=query_id_col, how='outer')
+        combined_data = pd.concat([combined_data, annotation_data.set_index(query_id_col)], axis=1, join='outer')
 
         # Remove duplicate samples within the same row and separate them with a semicolon
         combined_data['sample'] = combined_data['sample'].apply(lambda x: "; ".join(list(set(x))))
@@ -52,7 +55,7 @@ def combine_annotations(annotation_files, output_file):
 
         # Rearrange the columns to match the desired order
         output_columns_order = [query_id_col, 'sample'] + list(combined_data.columns.difference([query_id_col, 'sample', 'best_bitScore_col', 'best_bitScore']))
-        combined_data = combined_data[output_columns_order]
+        combined_data = combined_data[output_columns_order].reset_index()
 
         logging.info(f"Processed annotation file: {file_path}")
 
