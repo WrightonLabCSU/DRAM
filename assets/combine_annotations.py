4a183a35465e3134c277359bb2670d50371ddfe9
Reed Woyda <reedwoyda@gmail.com>
2024-01-08 14:50:09 -0700

    Modified kofam_formatter to clean up the kofam_EC column

--- assets/combine_annotations.py
+++ assets/combine_annotations.py
@@ -11,59 +11,46 @@ def combine_annotations(annotation_files, output_file):
     combined_data = pd.DataFrame()
 
     # Process the input annotation files
-    for i in range(0, len(annotation_files), 2):
-        sample = annotation_files[i]
-        file_path = annotation_files[i + 1]
-
-        # Extract sample names from the input
-        sample = sample.strip('\'"')
+    for annotation_file in annotation_files:
+        # Extract sample name from the input file path
+        sample = os.path.splitext(os.path.basename(annotation_file))[0]
 
         # Check if the file exists
-        if not os.path.exists(file_path):
-            raise ValueError(f"Could not find file: {file_path}")
+        if not os.path.exists(annotation_file):
+            raise ValueError(f"Could not find file: {annotation_file}")
 
         # Read each annotation file
-        logging.info(f"Processing annotation file: {file_path}")
+        logging.info(f"Processing annotation file: {annotation_file}")
 
         try:
-            # Read CSV with custom separator and without specifying names
-            annotation_data = pd.read_csv(file_path, sep=',', header=0)
+            # Read CSV without specifying names
+            annotation_data = pd.read_csv(annotation_file, header=0)
         except FileNotFoundError:
-            raise ValueError(f"Could not find file: {file_path}")
+            raise ValueError(f"Could not find file: {annotation_file}")
 
         # Print column names for each annotation file
         logging.info(f"Column names: {annotation_data.columns}")
 
-        # Make the script case-insensitive when checking for 'query_id'
-        query_id_col = [col for col in annotation_data.columns if col.lower() == 'query_id']
-        if not query_id_col:
-            raise ValueError(f"Column 'query_id' not found in the annotation file: {file_path}")
-        query_id_col = query_id_col[0]
+        # Find the '*_bitScore' column dynamically
+        bit_score_col = [col for col in annotation_data.columns if col.endswith('_bitScore')]
+        if not bit_score_col:
+            raise ValueError(f"No '*_bitScore' column found in the annotation file: {annotation_file}")
+        bit_score_col = bit_score_col[0]
 
-        # Keep the row with the highest bitScore for each query_id
-        annotation_data = annotation_data.sort_values(by=f'{sample}_bitScore', ascending=False).drop_duplicates('query_id')
+        # Find the row with the highest bitScore for each query_id
+        annotation_data = annotation_data.sort_values(by=bit_score_col, ascending=False).drop_duplicates('query_id')
 
         # Merge dataframes based on 'query_id' column
         if combined_data.empty:
             combined_data = annotation_data.copy()
         else:
-            common_cols = set(combined_data.columns) & set(annotation_data.columns)
-            merge_cols = ['query_id'] + list(common_cols - {'query_id'})
-            combined_data = pd.merge(combined_data, annotation_data, how='outer', on=merge_cols, suffixes=('_dbcan', f'_{sample}'))
+            combined_data = pd.concat([combined_data, annotation_data], ignore_index=True)
 
-        logging.info(f"Processed annotation file: {file_path} for sample: {sample}")
-
-    # Check if 'sample' column exists before saving to the output file
-    if 'sample' not in combined_data.columns:
-        raise ValueError("'sample' column not found in the combined data.")
+        logging.info(f"Processed annotation file: {annotation_file} for sample: {sample}")
 
     # Rearrange the columns to match the desired order
-    output_columns_order = ['query_id', 'sample'] + sorted([col for col in combined_data.columns if col not in ['query_id', 'sample']])
+    output_columns_order = ['query_id'] + sorted([col for col in combined_data.columns if col != 'query_id'])
     combined_data = combined_data[output_columns_order]
-
-    # Convert 'sample' column values to strings and join them with a semicolon
-    sample_cols = [col for col in combined_data.columns if col.startswith('sample')]
-    combined_data['sample'] = combined_data[sample_cols].apply(lambda row: "; ".join(map(str, filter(lambda x: pd.notna(x), row))), axis=1)
 
     # Save the combined data to the output file
     combined_data.to_csv(output_file, sep='\t', index=False)
@@ -75,8 +62,5 @@ if __name__ == '__main__':
     parser.add_argument('--output', help='Output file name', required=True)
 
     args = parser.parse_args()
-
-    # Remove square brackets and extra commas
-    args.annotations = [arg.strip("[],") for arg in args.annotations]
 
     combine_annotations(args.annotations, args.output)
