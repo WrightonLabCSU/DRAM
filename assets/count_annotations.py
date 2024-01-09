8c14fedf8bd758550e0e997c8a34cceb5f6f6002
Reed Woyda <reedwoyda@gmail.com>
2024-01-09 15:24:43 -0700

    Modified combine_annotations to accomodate database prefix in the col names

--- assets/count_annotations.py
+++ assets/count_annotations.py
@@ -9,19 +9,25 @@ id_columns = [col for col in data.columns if col.endswith("_id") and col != "que
 # Extract "sample" column
 samples = data['sample'].str.split('; ')
 
-# Create a new DataFrame for target IDs and samples
+# Create an empty DataFrame to store the target_id data
 target_id_data = pd.DataFrame()
 
-# For each column ending with "_id", explode the list and create separate rows
-for col in id_columns:
-    target_id_data = target_id_data.append(data[['sample', col]].explode(col))
+# Process the input annotation files
+for i in range(0, len(annotation_files), 2):
+    # Read the data from the annotation file
+    data = pd.read_csv(annotation_files[i + 1], sep='\t')
 
-# Rename the column containing values from "_id" columns to "target_id"
-target_id_data = target_id_data.rename(columns={0: 'target_id'})
+    # Identify columns containing database IDs by searching for names ending with "_id"
+    id_columns = [col for col in data.columns if col.endswith("_id") and col != "query_id"]
+
+    # Create a DataFrame with 'sample' and the current '_id' column exploded
+    exploded_data = pd.concat([data[['sample', col]].explode(col) for col in id_columns])
+
+    # Append the exploded data to the target_id_data DataFrame
+    target_id_data = pd.concat([target_id_data, exploded_data])
 
 # Create a new DataFrame for counting occurrences using groupby
-table = target_id_data.groupby(['target_id', 'sample']).size().unstack(fill_value=0)
+table = target_id_data.groupby(['sample', 'target_id']).size().unstack(fill_value=0)
 
 # Save the resulting table to a TSV file
 table.reset_index().to_csv("target_id_counts.tsv", sep='\t', index=False)
-
