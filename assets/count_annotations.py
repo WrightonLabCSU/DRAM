518b227d76375c477db61918f9337f1460425765
Reed Woyda <reedwoyda@gmail.com>
2024-01-09 15:23:22 -0700

    Modified combine_annotations to accomodate database prefix in the col names

--- assets/count_annotations.py
+++ assets/count_annotations.py
@@ -14,16 +14,13 @@ target_id_data = pd.DataFrame()
 
 # For each column ending with "_id", explode the list and create separate rows
 for col in id_columns:
-    target_id_data = target_id_data.append(data[col].str.split('; ').explode())
-
-# Combine the "sample" column with the target ID columns
-target_id_data['sample'] = samples.explode()
+    target_id_data = target_id_data.append(data[['sample', col]].explode(col))
 
 # Rename the column containing values from "_id" columns to "target_id"
 target_id_data = target_id_data.rename(columns={0: 'target_id'})
 
-# Create a new DataFrame for counting occurrences using crosstab
-table = pd.crosstab(index=target_id_data['target_id'], columns=target_id_data['sample']).fillna(0)
+# Create a new DataFrame for counting occurrences using groupby
+table = target_id_data.groupby(['target_id', 'sample']).size().unstack(fill_value=0)
 
 # Save the resulting table to a TSV file
 table.to_csv("target_id_counts.tsv", sep='\t')
