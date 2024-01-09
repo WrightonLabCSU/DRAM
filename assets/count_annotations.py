9d8398990f4543782d780b778d7682f598d4b722
Reed Woyda <reedwoyda@gmail.com>
2024-01-09 14:58:27 -0700

    Modified combine_annotations to accomodate database prefix in the col names

--- assets/count_annotations.py
+++ assets/count_annotations.py
@@ -3,16 +3,19 @@ import pandas as pd
 # Read the data from the combined_annotations.tsv file
 data = pd.read_csv("combined_annotations.tsv", sep='\t')
 
-# Split the "target_id" and "sample" columns by the delimiter "; " and create separate rows for each element
-data['target_id'] = data['target_id'].str.split('; ')
+# Identify columns containing database IDs by searching for names ending with "_id"
+id_columns = [col for col in data.columns if col.endswith("_id") and col != "query_id"]
+
+# Split the "query_id" and "sample" columns by the delimiter "; " and create separate rows for each element
+data['query_id'] = data['query_id'].str.split('; ')
 data['sample'] = data['sample'].str.split('; ')
 
 # Explode the lists to create separate rows for each element
-data = data.explode('target_id')
+data = data.explode('query_id')
 data = data.explode('sample')
 
-# Group by "target_id" and "sample," then count the occurrences
-table = data.groupby(['target_id', 'sample']).size().unstack(fill_value=0)
+# Group by "query_id" and "sample," then count the occurrences for each database ID column
+table = data.groupby(['query_id', 'sample'])[id_columns].count().fillna(0)
 
 # Save the resulting table to a TSV file
 table.to_csv("target_id_counts.tsv", sep='\t')
