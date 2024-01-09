8470bdca6c50ce900d67b8c2dc56972b9de9557b
Reed Woyda <reedwoyda@gmail.com>
2024-01-09 15:08:40 -0700

    Modified combine_annotations to accomodate database prefix in the col names

--- assets/count_annotations.py
+++ assets/count_annotations.py
@@ -3,7 +3,7 @@ import pandas as pd
 # Read the data from the combined_annotations.tsv file
 data = pd.read_csv("combined_annotations.tsv", sep='\t')
 
-# Identify columns containing database IDs by searching for names ending with "_id" (excluding "query_id")
+# Identify columns containing database IDs by searching for names ending with "_id"
 id_columns = [col for col in data.columns if col.endswith("_id") and col != "query_id"]
 
 # Extract "sample" column
@@ -20,12 +20,10 @@ for col in id_columns:
 target_id_data['sample'] = samples.explode()
 
 # Create a new DataFrame for counting occurrences
-occurrences_data = target_id_data.groupby(['sample'] + id_columns, as_index=False).size().reset_index()
-occurrences_data = occurrences_data.rename(columns={0: 'occurrences'})
+occurrences_data = target_id_data.groupby(['sample'] + id_columns).size().reset_index(name='occurrences')
 
 # Use the first column from target_id_data as the target_id values
 occurrences_data['target_id'] = target_id_data.iloc[:, 0]
-
 
 # Pivot the table to have samples as columns and target_ids as rows with the count of occurrences
 table = occurrences_data.pivot(index='target_id', columns='sample', values='occurrences').fillna(0)
