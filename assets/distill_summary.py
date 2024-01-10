1b243fbf20fbed60c7bfe1de45f6a39277cc3b6b
Reed Woyda <reedwoyda@gmail.com>
2024-01-10 09:26:20 -0700

    Modified combine_annotations to accomodate database prefix in the col names

--- assets/distill_summary.py
+++ assets/distill_summary.py
@@ -1,27 +1,28 @@
 import argparse
 import pandas as pd
 
-def distill_summary(combined_annotations_file, genome_summary_file, output_file):
-    # Read input files into DataFrames
+def distill_summary(combined_annotations_file, genome_summary_form_file, output_file):
+    # Read input files
     combined_annotations = pd.read_csv(combined_annotations_file, sep='\t')
-    genome_summary = pd.read_csv(genome_summary_file, sep='\t')
+    genome_summary_form = pd.read_csv(genome_summary_form_file, sep='\t')
 
-    # Initialize the output DataFrame
-    distill_summary_df = pd.DataFrame(columns=['gene_id', 'query_id', 'sample'])
+    # Extract gene_id, query_id, and sample information
+    distilled_summary = pd.DataFrame(columns=['gene_id', 'query_id', 'sample'])
 
-    # Iterate through gene_ids in genome_summary_file
-    for gene_id in genome_summary['gene_id']:
-        # Find matching row in combined_annotations based on _id columns
-        match_row = combined_annotations[combined_annotations.filter(like='_id').eq(gene_id).any(axis=1)]
+    for index, row in genome_summary_form.iterrows():
+        gene_id = row['gene_id']
 
-        if not match_row.empty:
-            # Extract relevant information and append to distill_summary_df
-            query_id = match_row['query_id'].iloc[0]
-            sample = match_row['sample'].iloc[0]
-            distill_summary_df = distill_summary_df.append({'gene_id': gene_id, 'query_id': query_id, 'sample': sample}, ignore_index=True)
+        # Check if the gene_id is present in combined_annotations
+        if f"{gene_id}_id" in combined_annotations.columns:
+            matching_data = combined_annotations[['query_id', 'sample', f"{gene_id}_id"]]
+            matching_data = matching_data.rename(columns={f"{gene_id}_id": 'query_id'})
+            matching_data['gene_id'] = gene_id
 
-    # Write the result to the output file
-    distill_summary_df.to_csv(output_file, sep='\t', index=False)
+            # Append matching data to the distilled_summary dataframe
+            distilled_summary = pd.concat([distilled_summary, matching_data], ignore_index=True)
+
+    # Write the distilled summary to the output file
+    distilled_summary.to_csv(output_file, sep='\t', index=False)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate distill summary')
@@ -31,5 +32,4 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    # Call the distill_summary function with the provided arguments
     distill_summary(args.combined_annotations, args.genome_summary_form, args.output)
