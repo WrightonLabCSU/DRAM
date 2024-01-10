1937dcfe1c3780800ad4f4006e515f49eff18ae4
Reed Woyda <reedwoyda@gmail.com>
2024-01-10 08:46:37 -0700

    Modified combine_annotations to accomodate database prefix in the col names

--- assets/distill_summary.py
+++ assets/distill_summary.py
@@ -28,14 +28,14 @@ def distill_summary(combined_annotations, genome_summary_form, target_id_counts,
     id_columns_combined.append('gene_id')
 
     # Merge combined_data with genome_summary and additional modules based on columns ending with "_id"
-    merged_data = pd.merge(combined_data, genome_summary, left_on=id_columns_combined, right_on='gene_id', how='inner')
-    merged_data = pd.merge(merged_data, additional_modules_combined, left_on=id_columns_combined, right_on=('add_module1', 'gene_id'), how='outer')
+    merged_data = pd.merge(combined_data, genome_summary, left_on=id_columns_combined, right_on=['gene_id'] * len(id_columns_combined), how='left')
+    merged_data = pd.merge(merged_data, additional_modules_combined, left_on=id_columns_combined, right_on=['add_module1', 'gene_id'] * len(id_columns_combined), how='left')
 
     # Create the distill_summary dataframe
-    distill_summary_df = merged_data[['query_id', 'sample', 'gene_id'] + list(genome_summary.columns[1:])]
+    distill_summary = merged_data[['query_id', 'sample', 'gene_id'] + list(genome_summary.columns[1:])]
 
     # Write the distill_summary dataframe to the output file
-    distill_summary_df.to_csv(output, sep='\t', index=False)
+    distill_summary.to_csv(output, sep='\t', index=False)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate distill summary')
