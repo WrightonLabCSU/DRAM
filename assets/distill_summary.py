2d550096b05e56d652a52a6fa9bc913bf197a24a
Reed Woyda <reedwoyda@gmail.com>
2024-01-10 11:07:33 -0700

    Modified combine_annotations to accomodate database prefix in the col names

--- assets/distill_summary.py
+++ assets/distill_summary.py
@@ -65,9 +65,16 @@ def distill_summary(combined_annotations_path, genome_summary_form_path, output_
                     # Add values from add_moduleX columns, concatenate values with "; "
                     for add_module_col in add_module_data.columns[1:]:
                         if add_module_col in distill_summary_df.columns:
-                            # Convert values to strings before concatenating
-                            distill_summary_df.at[distill_summary_df.index[-1], add_module_col] += f'; {str(row[add_module_col])}'
+                            existing_value = distill_summary_df.at[distill_summary_df.index[-1], add_module_col]
+                            new_value = str(row[add_module_col])
+
+                            # Check if the existing value is not NaN (numeric), then concatenate with "; "
+                            if pd.notna(existing_value):
+                                distill_summary_df.at[distill_summary_df.index[-1], add_module_col] = f'{existing_value}; {new_value}'
+                            else:
+                                distill_summary_df.at[distill_summary_df.index[-1], add_module_col] = new_value
                         else:
+                            # Convert values to strings before assigning
                             distill_summary_df.at[distill_summary_df.index[-1], add_module_col] = str(row[add_module_col])
 
     # Write the output to a TSV file
