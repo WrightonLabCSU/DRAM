b101b54dbf9e921ea82838981bc1a42bfd4f34b4
Reed Woyda <reedwoyda@gmail.com>
2024-01-10 08:04:18 -0700

    Modified combine_annotations to accomodate database prefix in the col names

--- assets/distill_summary.py
+++ assets/distill_summary.py
@@ -5,7 +5,13 @@ def distill_summary(combined_annotations, genome_summary_form, target_id_counts,
     # Read input files
     combined_annotations_data = pd.read_csv(combined_annotations, sep='\t')
     genome_summary_form_data = pd.read_csv(genome_summary_form, sep='\t')
-    target_id_counts_data = pd.read_csv(target_id_counts, sep='\t')
+
+    # Select columns ending in '_id' for gene_id
+    id_columns = [col for col in combined_annotations_data.columns if col.endswith('_id')]
+    combined_annotations_data['gene_id'] = combined_annotations_data[id_columns].apply(lambda x: '; '.join(x.dropna()), axis=1)
+
+    # Merge combined_annotations with genome_summary_form on gene_id
+    merged_data = pd.merge(combined_annotations_data, genome_summary_form_data, left_on=['gene_id'], right_on=['gene_id'], how='left')
 
     # Create a dictionary to store additional modules data
     additional_modules = {}
@@ -14,25 +20,15 @@ def distill_summary(combined_annotations, genome_summary_form, target_id_counts,
             additional_module_data = pd.read_csv(add_module_file, sep='\t')
             additional_modules[f'add_module{i}'] = additional_module_data
 
-    # Select columns ending in '_id' for gene_id
-    id_columns = [col for col in combined_annotations_data.columns if col.endswith('_id')]
-    combined_annotations_data['gene_id'] = combined_annotations_data[id_columns].apply(lambda x: '; '.join(x.dropna()), axis=1)
-
-    # Merge combined_annotations with genome_summary_form on both query_id and gene_id
-    merged_data = pd.merge(combined_annotations_data, genome_summary_form_data, left_on=['query_id', 'gene_id'], right_on=['query_id', 'gene_id'], how='left')
-
     # Select relevant columns from merged_data
-    output_columns = ['query_id', 'sample', 'gene_id'] + list(genome_summary_form_data.columns[2:])
-
-    # Merge with target_id_counts on 'gene_id' column
-    final_data = pd.merge(merged_data[output_columns], target_id_counts_data, left_on='gene_id', right_on='target_id', how='left').drop(columns=['target_id', 'BB02', 'small_sample-fasta'])
+    output_columns = ['query_id', 'sample', 'gene_id'] + list(genome_summary_form_data.columns[1:])
 
     # Append additional modules data
     for key, additional_module_data in additional_modules.items():
-        final_data = pd.merge(final_data, additional_module_data, on='gene_id', how='left', suffixes=('', f'_{key}'))
+        merged_data = pd.merge(merged_data, additional_module_data, on='gene_id', how='left', suffixes=('', f'_{key}'))
 
     # Write the final result to the output file
-    final_data.to_csv(output, sep='\t', index=False)
+    merged_data.to_csv(output, sep='\t', index=False)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate distill summary')
