be95278d73de6e355e528fe84950f06f64a60dd1
Reed Woyda <reedwoyda@gmail.com>
2024-01-10 09:10:25 -0700

    Modified combine_annotations to accomodate database prefix in the col names

--- assets/distill_summary.py
+++ assets/distill_summary.py
@@ -1,50 +1,27 @@
 import argparse
 import pandas as pd
-import os
 
-def distill_summary(combined_annotations, genome_summary_form, output, add_modules):
-    # Read input files
+def distill_summary(combined_annotations, genome_summary_form, output_file):
+    # Read the input files using pandas
     combined_data = pd.read_csv(combined_annotations, sep='\t')
-    genome_data = pd.read_csv(genome_summary_form, sep='\t')
+    genome_summary_data = pd.read_csv(genome_summary_form, sep='\t')
 
-    # Initialize an empty DataFrame for additional module data
-    additional_modules = pd.DataFrame()
+    # Select relevant columns from genome_summary_data for the output
+    output_columns = [col for col in genome_summary_data.columns if col != 'gene_id']
 
-    # Read additional module files if provided
-    for i, add_module_file in enumerate(add_modules, start=1):
-        if add_module_file and add_module_file != 'empty':
-            additional_module_data = pd.read_csv(add_module_file, sep='\t')
-            additional_modules[f'add_module{i}'] = additional_module_data
+    # Merge data based on 'gene_id' columns
+    merged_data = pd.merge(combined_data, genome_summary_data, left_on='gene_id', right_on='gene_id', how='inner')
 
-    # Merge dataframes based on gene_id
-    merged_data = pd.merge(combined_data, genome_data, left_on='gene_id', right_on='gene_id', how='inner')
-
-    # Concatenate additional module data, if available
-    if not additional_modules.empty:
-        merged_data = pd.concat([merged_data, additional_modules], axis=1)
-
-    # Check if output directory exists, create if not
-    output_dir = os.path.dirname(output)
-    if output_dir and not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
-    # Write the result to the output file
-    merged_data.to_csv(output, sep='\t', index=False)
+    # Write the distilled summary to the output file
+    merged_data[output_columns].to_csv(output_file, sep='\t', index=False)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate distill summary')
     parser.add_argument('--combined_annotations', required=True, help='Path to the combined annotations file')
     parser.add_argument('--genome_summary_form', required=True, help='Path to the genome summary file')
-    parser.add_argument('--target_id_counts', required=True, help='Path to the target_id_counts file')
     parser.add_argument('--output', required=True, help='Path to the output file')
-    parser.add_argument('--add_module1', required=False, help='Path to the additional module1 file')
-    parser.add_argument('--add_module2', required=False, help='Path to the additional module2 file')
-    parser.add_argument('--add_module3', required=False, help='Path to the additional module3 file')
-    parser.add_argument('--add_module4', required=False, help='Path to the additional module4 file')
-    parser.add_argument('--add_module5', required=False, help='Path to the additional module5 file')
 
     args = parser.parse_args()
 
-    # Call the distill_summary function
-    distill_summary(args.combined_annotations, args.genome_summary_form, args.output,
-                    [args.add_module1, args.add_module2, args.add_module3, args.add_module4, args.add_module5])
+    # Call the distill_summary function with the provided arguments
+    distill_summary(args.combined_annotations, args.genome_summary_form, args.output)
--- modules/distill/distill_summary.nf
+++ modules/distill/distill_summary.nf
@@ -1,9 +1,9 @@
 process DISTILL_SUMMARY {
 
     input:
-    file( combined_annotations )
-    file( target_id_counts )
-    file( ch_distill_summary_script )
+    file combined_annotations
+    file genome_summary_form
+    file ch_distill_summary_script
 
     output:
     path("genome_summary.tsv"), emit: metab_summ_simple
@@ -15,24 +15,39 @@ process DISTILL_SUMMARY {
   
     # Define the log file path
     log_file="logs/distill.log"
-  
-    if [[ "${params.add_module1}" != "empty" || "${params.add_module2}" != "empty" || "${params.add_module3}" != "empty" || "${params.add_module4}" != "empty" || "${params.add_module5}" != "empty" ]]; then
-        python "${ch_distill_summary_script}" \
-            --combined_annotations "${combined_annotations}" \
-            --genome_summary_form "${params.genome_summary_form}" \
-            --target_id_counts "${target_id_counts}" \
-            --output "genome_summary.tsv" \
-            --add_module1 "${params.add_module1}" \
-            --add_module2 "${params.add_module2}" \
-            --add_module3 "${params.add_module3}" \
-            --add_module4 "${params.add_module4}" \
-            --add_module5 "${params.add_module5}" >> "\$log_file" 2>&1
-    else
-        python "${ch_distill_summary_script}" \
-            --combined_annotations "${combined_annotations}" \
-            --genome_summary_form "${params.genome_summary_form}" \
-            --target_id_counts "${target_id_counts}" \
-            --output "genome_summary.tsv" >> "\$log_file" 2>&1
-    fi
+
+    python "${ch_distill_summary_script}" \
+        --combined_annotations "${combined_annotations}" \
+        --genome_summary_form "${genome_summary_form}" \
+        --output "genome_summary.tsv" >> "\$log_file" 2>&1
     """
 }
+
+
+/*
+process DISTILL_SUMMARY {
+
+    input:
+    file combined_annotations
+    file genome_summary_form
+    file ch_distill_summary_script
+
+    output:
+    path("genome_summary.tsv"), emit: metab_summ_simple
+
+    script:
+    """
+    # Create a log directory if it doesn't exist
+    mkdir -p logs
+  
+    # Define the log file path
+    log_file="logs/distill.log"
+
+    python "${ch_distill_summary_script}" \
+        --combined_annotations "${combined_annotations}" \
+        --genome_summary_form "${genome_summary_form}" \
+        --output "genome_summary.tsv" >> "\$log_file" 2>&1
+    """
+}
+
+*/
