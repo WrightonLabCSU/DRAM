0213de9df39fd14e0142f3c9f5596575a9190eb2
Reed Woyda <reedwoyda@gmail.com>
2024-01-10 09:33:51 -0700

    Modified combine_annotations to accomodate database prefix in the col names

--- assets/distill_summary.py
+++ assets/distill_summary.py
@@ -6,7 +6,7 @@ def distill_summary(combined_annotations_file, genome_summary_form_file, output_
     combined_annotations = pd.read_csv(combined_annotations_file, sep='\t')
     genome_summary_form = pd.read_csv(genome_summary_form_file, sep='\t')
 
-    # Merge DataFrames based on gene_id
+    # Merge DataFrames based on gene_id and query_id
     merged_data = pd.merge(genome_summary_form, combined_annotations, left_on='gene_id', right_on='query_id', how='inner')
 
     # Extract relevant columns
