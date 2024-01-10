637c8d23d9cbca20e9d1b15b5c3fbd2cead67abd
Reed Woyda <reedwoyda@gmail.com>
2024-01-10 10:15:39 -0700

    Modified combine_annotations to accomodate database prefix in the col names

--- assets/distill_summary.py
+++ assets/distill_summary.py
@@ -1,7 +1,7 @@
 import argparse
 import pandas as pd
 
-def distill_summary(combined_annotations_path, genome_summary_form_path, output_path, *add_module_files):
+def distill_summary(combined_annotations_path, genome_summary_form_path, output_path):
     # Read input files
     combined_annotations = pd.read_csv(combined_annotations_path, sep='\t')
     genome_summary_form = pd.read_csv(genome_summary_form_path, sep='\t')
@@ -24,14 +24,14 @@ def distill_summary(combined_annotations_path, genome_summary_form_path, output_
 
     # Iterate through gene_id values in genome_summary_form
     for gene_id in genome_summary_form['gene_id']:
-        # Search for a match in combined_annotations columns ending in "_id"
+        # Search for matches in combined_annotations columns ending in "_id"
         match_columns = [col for col in combined_annotations.columns if col.endswith('_id') and col != 'query_id']
 
         for column in match_columns:
             # Check for matches
             match_rows = combined_annotations[combined_annotations[column] == gene_id]
 
-            for index, match_row in match_rows.iterrows():
+            for _, match_row in match_rows.iterrows():
                 # Add matching information to the output DataFrame
                 distill_summary_df = distill_summary_df.append({
                     'gene_id': gene_id,
@@ -47,7 +47,7 @@ def distill_summary(combined_annotations_path, genome_summary_form_path, output_
 
                 # Add values from selected columns in combined_annotations
                 for col in combined_columns_to_add:
-                    distill_summary_df.at[distill_summary_df.index[-1], col] += str(match_row[col]) + ';'
+                    distill_summary_df.at[distill_summary_df.index[-1], col] = match_row[col]
 
     # Write the output to a TSV file
     distill_summary_df.to_csv(output_path, sep='\t', index=False)
@@ -57,14 +57,8 @@ if __name__ == '__main__':
     parser.add_argument('--combined_annotations', required=True, help='Path to the combined annotations file')
     parser.add_argument('--genome_summary_form', required=True, help='Path to the genome summary file')
     parser.add_argument('--output', required=True, help='Path to the output file')
-    parser.add_argument('--add_module1', default='empty', help='Path to additional module 1 file')
-    parser.add_argument('--add_module2', default='empty', help='Path to additional module 2 file')
-    parser.add_argument('--add_module3', default='empty', help='Path to additional module 3 file')
-    parser.add_argument('--add_module4', default='empty', help='Path to additional module 4 file')
-    parser.add_argument('--add_module5', default='empty', help='Path to additional module 5 file')
 
     args = parser.parse_args()
 
     # Call the distill_summary function with provided arguments
-    distill_summary(args.combined_annotations, args.genome_summary_form, args.output,
-                    args.add_module1, args.add_module2, args.add_module3, args.add_module4, args.add_module5)
+    distill_summary(args.combined_annotations, args.genome_summary_form, args.output)
