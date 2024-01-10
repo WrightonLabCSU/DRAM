a250e4f471120a5c20d47bae39920e668645c3ff
Reed Woyda <reedwoyda@gmail.com>
2024-01-09 19:33:36 -0700

    Modified combine_annotations to accomodate database prefix in the col names

--- assets/distill_summary.py
+++ assets/distill_summary.py
@@ -14,12 +14,12 @@ def distill_summary(combined_annotations, genome_summary_form, target_id_counts,
             additional_module_data = pd.read_csv(add_module_file, sep='\t')
             additional_modules[f'add_module{i}'] = additional_module_data
 
+    # Select columns ending in '_id' for gene_id
+    id_columns = [col for col in combined_annotations_data.columns if col.endswith('_id')]
+    combined_annotations_data['gene_id'] = combined_annotations_data[id_columns].apply(lambda x: '; '.join(x.dropna()), axis=1)
+
     # Merge combined_annotations with genome_summary_form on gene_id
     merged_data = pd.merge(combined_annotations_data, genome_summary_form_data, on='gene_id', how='left')
-
-    # Concatenate columns ending in '_id' to form gene_id
-    id_columns = [col for col in combined_annotations_data.columns if col.endswith('_id')]
-    merged_data['gene_id'] = merged_data[id_columns].apply(lambda x: '; '.join(x.dropna()), axis=1)
 
     # Select relevant columns from merged_data
     output_columns = ['query_id', 'sample', 'gene_id'] + list(genome_summary_form_data.columns[1:])
