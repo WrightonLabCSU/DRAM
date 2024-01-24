1bdfe36381bbc99335b6a1edbacdc65d50dd47c3
Reed Woyda <reedwoyda@gmail.com>
2024-01-24 14:16:55 -0700

    Modified parse_hmmsearch to include strandedness

--- assets/parse_hmmsearch.py
+++ assets/parse_hmmsearch.py
@@ -6,20 +6,25 @@ HMMSCAN_ALL_COLUMNS = ['query_id', 'query_ascession', 'query_length', 'target_id
 HMMSCAN_COLUMN_TYPES = [str, str, int, str, str, int, float, float, float, int, int, float, float, float, float, int, int, int, int, int, int, float, str]
 
 def parse_hmmsearch_domtblout(file):
-    df_lines = list()
+    df_lines = []
     for line in open(file):
         if not line.startswith('#'):
             line = line.split()
             line = line[:22] + [' '.join(line[22:])]
             df_lines.append(line)
-    hmmsearch_frame = pd.DataFrame(df_lines, columns=HMMSCAN_ALL_COLUMNS)
-    for i, column in enumerate(hmmsearch_frame.columns):
-        hmmsearch_frame[column] = hmmsearch_frame[column].astype(HMMSCAN_COLUMN_TYPES)
+
+    column_data = {col: [] for col in HMMSCAN_ALL_COLUMNS}
+    for line in df_lines:
+        for i, col in enumerate(HMMSCAN_ALL_COLUMNS):
+            column_data[col].append(HMMSCAN_COLUMN_TYPES[i](line[i]))
+
+    hmmsearch_frame = pd.DataFrame(column_data)
     
     # Add the "strandedness" column
     hmmsearch_frame['strandedness'] = hmmsearch_frame.apply(lambda row: 1 if row['description'].endswith('+') else -1, axis=1)
-    
+
     return hmmsearch_frame
+
 
 if __name__ == '__main__':
     input_file = sys.argv[1]
