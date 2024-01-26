e90131bd5b2e97d30f6f1eb4b6a13b3f97705ef9
Reed Woyda <reedwoyda@gmail.com>
2024-01-26 10:47:49 -0700

    Modified distill to use the command line parameters: --distill_topic, --distill_ecosys and --distill_custom.

--- assets/distill_summary.py
+++ assets/distill_summary.py
@@ -2,31 +2,33 @@ import pandas as pd
 import os
 import argparse
 
-def distill_summary(combined_annotations_path, distill_sheets, output_path):
-    # Print the contents of distill_sheets
-    print("distill_sheets:", distill_sheets)
-
+def distill_summary(combined_annotations_path, distill_sheets_file, output_path):
     # Read the combined_annotations file
     combined_annotations_df = pd.read_csv(combined_annotations_path, sep='\t')
 
     # Initialize an empty DataFrame to store the distill summary
     distill_summary_df = pd.DataFrame(columns=combined_annotations_df.columns)
 
+    # Read the paths to distill sheets from the text file
+    with open(distill_sheets_file, 'r') as file:
+        distill_sheets = [line.strip() for line in file.readlines() if line.strip()]
+
     # Process each distill sheet
     for distill_sheet in distill_sheets:
-        # Read the distill sheet
-        distill_df = pd.read_csv(distill_sheet, sep='\t')
+        # Split the comma-separated paths into individual paths
+        distill_sheet_paths = distill_sheet.split(',')
 
-        # Use the values in the 'topic_ecosystem' column as the sheet names
-        topic_ecosystem = distill_df['topic_ecosystem'].iloc[0]
+        for sheet_path in distill_sheet_paths:
+            # Read the distill sheet
+            distill_df = pd.read_csv(sheet_path, sep='\t')
 
-        # Process each gene_id
-        for gene_id in distill_df['gene_id']:
-            # Filter rows with the current gene_id
-            gene_rows = distill_df[distill_df['gene_id'] == gene_id]
+            # Process each gene_id
+            for gene_id in distill_df['gene_id']:
+                # Filter rows with the current gene_id
+                gene_rows = distill_df[distill_df['gene_id'] == gene_id]
 
-            # Concatenate the rows to the distill summary DataFrame
-            distill_summary_df = pd.concat([distill_summary_df, gene_rows], ignore_index=True)
+                # Concatenate the rows to the distill summary DataFrame
+                distill_summary_df = pd.concat([distill_summary_df, gene_rows], ignore_index=True)
 
     # Save the distill summary to the specified output path
     distill_summary_df.to_csv(output_path, sep='\t', index=False)
@@ -39,8 +41,4 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    # Read the paths to distill sheets from the text file
-    with open(args.distill_sheets, 'r') as file:
-        distill_sheets = [line.strip() for line in file.readlines() if line.strip()]
-
-    distill_summary(args.combined_annotations, distill_sheets, args.output)
+    distill_summary(args.combined_annotations, args.distill_sheets, args.output)
