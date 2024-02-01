d5cd015bbce0da47d42b8ce16f6232c56dbc9290
Reed Woyda <reedwoyda@gmail.com>
2024-02-01 14:17:20 -0700

    Adding genome_stats sheet to distillate output

--- assets/generate_multi_sheet_xlsx.py
+++ assets/generate_multi_sheet_xlsx.py
@@ -63,11 +63,11 @@ def generate_multi_sheet_xlsx(input_file, rrna_file, trna_file, combined_annotat
                         print(f"\nUpdating RNA column: {rna_type}")
                         print(f"Values to be populated: {joined_values}")
                         print(f"Column Index in genome_stats: {col_idx}")
-                        for row in gs_sheet.iter_rows(min_row=2, max_row=gs_sheet.max_row, min_col=column_names.index(col) + 1, max_col=column_names.index(col) + 1):
+                        for row in gs_sheet.iter_rows(min_row=2, max_row=gs_sheet.max_row, min_col=col_idx, max_col=col_idx):
                             if row[0].value == sample:
                                 row_idx = row[0].row
-                                gs_sheet.cell(row=row_idx, column=column_names.index(col) + 1).value = joined_values
-                                print(f"Updated value at row {row_idx}, column {column_names.index(col) + 1}")
+                                gs_sheet.cell(row=row_idx, column=col_idx).value = joined_values
+                                print(f"Updated value at row {row_idx}, column {col_idx}")
 
     # Print completeness and contamination values
     print("\nCompleteness and Contamination values:")
