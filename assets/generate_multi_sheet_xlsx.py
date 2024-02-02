db3b37402fdf36cdee5da256a40853863084a7c1
Reed Woyda <reedwoyda@gmail.com>
2024-02-02 08:43:22 -0700

    Adding genome_stats sheet to distillate output

--- assets/generate_multi_sheet_xlsx.py
+++ assets/generate_multi_sheet_xlsx.py
@@ -65,9 +65,6 @@ def generate_multi_sheet_xlsx(input_file, rrna_file, trna_file, combined_annotat
 
     # Update RNA columns dynamically
     for rna_type in unique_rna_types:
-        # Filter rrna_data based on rna_type
-        filtered_rrna_data = rrna_data[rrna_data['type'] == rna_type]
-
         # Find the corresponding column index in the genome_stats sheet
         col_idx = column_names.index(rna_type) + 1
         print(f"\nUpdating RNA column: {rna_type}")
@@ -76,7 +73,7 @@ def generate_multi_sheet_xlsx(input_file, rrna_file, trna_file, combined_annotat
         # Iterate over samples
         for idx, sample in enumerate(unique_samples, start=2):
             # Extract relevant data for the current sample and rna_type
-            sample_rrna_data = filtered_rrna_data[filtered_rrna_data['sample'] == sample]
+            sample_rrna_data = rrna_data[(rrna_data['sample'] == sample) & (rrna_data['type'] == rna_type)]
 
             # Check if sample_rrna_data is not empty
             if not sample_rrna_data.empty:
@@ -89,19 +86,9 @@ def generate_multi_sheet_xlsx(input_file, rrna_file, trna_file, combined_annotat
                 # Join multiple values with "; "
                 joined_values = "; ".join(values)
 
-                # Find the corresponding row index for the sample
-                for row in gs_sheet.iter_rows(min_row=2, max_row=gs_sheet.max_row, min_col=1, max_col=1):
-                    if row[0].value == sample:
-                        row_idx = row[0].row
-                        break
-
                 # Update the value in the correct cell
-                if rna_type == 'tRNA':
-                    gs_sheet.cell(row=row_idx, column=len(gs_sheet[1])).value = joined_values
-                    print(f"Updated tRNA count at row {row_idx}, column {len(gs_sheet[1])}")
-                else:
-                    gs_sheet.cell(row=row_idx, column=col_idx).value = joined_values
-                    print(f"Updated value at row {row_idx}, column {col_idx}")
+                gs_sheet.cell(row=idx, column=col_idx).value = joined_values
+                print(f"Updated value at row {idx}, column {col_idx}")
             else:
                 print(f"Sample {sample} has no data for {rna_type}")
 
