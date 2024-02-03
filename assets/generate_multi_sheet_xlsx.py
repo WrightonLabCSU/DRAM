847b0c08d578dac1b941d45d38e5c49514a407c4
Reed Woyda <reedwoyda@gmail.com>
2024-02-03 15:32:56 -0700

    Added flexibility for distill not require bin quality and taxa columns in annots.

--- assets/generate_multi_sheet_xlsx.py
+++ assets/generate_multi_sheet_xlsx.py
@@ -165,6 +165,8 @@ def generate_multi_sheet_xlsx(input_file, rrna_file, trna_file, combined_annotat
         # Append data rows to the worksheet
         for r_idx, row in enumerate(sheet_rows, 1):
             # Check if "potential_amg" column exists and if the value is available for this gene_id
+            print(f"Checking row['gene_id']: {row['gene_id']} in data['gene_id']: {data['gene_id']}")
+
             if 'potential_amg' in data.columns and any(data['gene_id'] == row['gene_id']):
                 potential_amg_value = data[data['gene_id'] == row['gene_id']]['potential_amg'].iloc[0]
                 row_data = [row['gene_id'], row['gene_description'], row['pathway'], row['topic_ecosystem'],
