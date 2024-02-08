6659eb5001dc76ccbccd6abf7c2fe73f752d1f08
Reed Woyda <reedwoyda@gmail.com>
2024-02-08 13:34:35 -0700

    Adding merge annotations process

--- assets/distill_summary.py
+++ assets/distill_summary.py
@@ -60,6 +60,8 @@ def distill_summary(combined_annotations_path, target_id_counts_df, output_path)
                         if matched_indices.any():
                             # Create a new row for each unique gene_id found
                             for combined_id in combined_annotations_df.loc[matched_indices, col.replace('_EC', '_id')]:
+                                if gene_id in combined_annotations_df[col.replace('_EC', '_id')].values:
+                                    gene_description += f'; {gene_id}'
                                 row_data = {'gene_id': combined_id,
                                             'gene_description': gene_description,
                                             'pathway': pathway,
