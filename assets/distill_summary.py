a65882ab32529f4ee27377bbf92ac2107b02a3ab
Reed Woyda <reedwoyda@gmail.com>
2024-02-09 15:30:46 -0700

    Adding merge annotations process

--- assets/distill_summary.py
+++ assets/distill_summary.py
@@ -124,8 +124,10 @@ def distill_summary(combined_annotations_path, target_id_counts_df, output_path)
                                     'category': category,
                                     'subcategory': subcategory,
                                     'associated_EC': associated_ec,  # This will be set if there's a partial match
-                                    **additional_cols.to_dict() if additional_cols is not None else {}
                                 }
+                                if additional_cols is not None:
+                                    row_data.update(additional_cols.to_dict())
+
                                 for id_col in combined_annotations_df.filter(like='_id').columns:
                                     if id_col != 'query_id':
                                         gene_id_value = combined_annotations_df.at[idx, id_col]
