8406237a31bbf03249d1a670f3a6f3bf75b4fe5a
Reed Woyda <reedwoyda@gmail.com>
2024-02-14 11:43:30 -0700

    Adding in camper, cant_hyd and methyl MMseqs database along with the mmseqs annotation.

--- assets/mmseqs_add_descriptions.py
+++ assets/mmseqs_add_descriptions.py
@@ -6,27 +6,28 @@ def main(sample, db_name, descriptions_path, bit_score_threshold):
     input_path = f"mmseqs_out/{sample}_mmseqs_{db_name}.tsv"
     output_path = f"mmseqs_out/{sample}_mmseqs_{db_name}_formatted.csv"
 
-    print("Input path:", input_path)
-    print("Output path:", output_path)
+    print(f"Input path: {input_path}")
+    print(f"Output path: {output_path}")
 
     # Load MMseqs output into DataFrame
-    print("Loading MMseqs output...")
     mmseqs_columns = ['query_id', 'target_id', 'pident', 'alnlen', 'mismatch', 'gapopen', 'qstart', 'qend', 'sstart', 'send', 'evalue', 'bitscore']
     df_mmseqs = pd.read_csv(input_path, sep='\t', names=mmseqs_columns)
 
+    print("Loading MMseqs output...")
+
     # Add additional columns for start and end positions, and format target_id with db_name
-    print("Processing MMseqs output...")
     df_mmseqs['start_position'] = df_mmseqs['qstart']
     df_mmseqs['end_position'] = df_mmseqs['qend']
     df_mmseqs[f"{db_name}_id"] = df_mmseqs['target_id']
     df_mmseqs[f"{db_name}_bitScore"] = df_mmseqs['bitscore']
     df_mmseqs_final = df_mmseqs[['query_id', 'start_position', 'end_position', f"{db_name}_id", f"{db_name}_bitScore"]]
 
+    print("Processing MMseqs output...")
+
     # Check if db_descriptions is not 'NULL'
-    print("Checking descriptions...")
     with open(descriptions_path, 'r') as file:
-        first_line = next(file).strip()
-        print("First line of descriptions:", first_line)
+        first_line = file.readline().strip()
+        print(f"First line of descriptions: {first_line}")
         if first_line.upper() != 'NULL':
             print("Descriptions file is not NULL. Processing...")
             file.seek(0)
@@ -34,13 +35,9 @@ def main(sample, db_name, descriptions_path, bit_score_threshold):
             # Process descriptions and merge...
             # Rest of the processing as before...
         else:
-            print("Descriptions file is NULL. Saving MMseqs output...")
+            print("Descriptions file is NULL. Skipping processing.")
             df_mmseqs.to_csv(output_path, index=False)
 
 if __name__ == "__main__":
     sample, db_name, descriptions_path, bit_score_threshold = sys.argv[1:]
-    print("Sample:", sample)
-    print("DB Name:", db_name)
-    print("Descriptions Path:", descriptions_path)
-    print("Bit Score Threshold:", bit_score_threshold)
     main(sample, db_name, descriptions_path, bit_score_threshold)
