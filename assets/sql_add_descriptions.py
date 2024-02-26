d26ea03028094daab49d82408e16c3fa163ab20e
Reed Woyda <reedwoyda@gmail.com>
2024-02-26 12:34:05 -0700

    Added functionality for adding db descriptions via sql database

--- assets/sql_add_descriptions.py
+++ assets/sql_add_descriptions.py
@@ -3,11 +3,14 @@ import sqlite3
 from concurrent.futures import ThreadPoolExecutor
 import argparse
 
-def fetch_descriptions(chunk, db_name, conn):
+def fetch_descriptions(chunk, db_name, db_file):
     # Function to fetch descriptions based on IDs from the specified table
     table_name = f"{db_name}_description"
     ids_column = f"{db_name}_id"
     descriptions_column = f"{db_name}_description"
+    
+    # Establish connection to SQLite database
+    conn = sqlite3.connect(db_file)
     
     ids = chunk[ids_column].unique()
     query = f"SELECT {ids_column}, {descriptions_column} FROM {table_name} WHERE {ids_column} IN ({','.join(['?'] * len(ids))})"
@@ -19,6 +22,9 @@ def fetch_descriptions(chunk, db_name, conn):
     descriptions_dict = {row[0]: row[1] for row in results}
     chunk[f"{db_name}_description"] = chunk[ids_column].map(descriptions_dict)
     
+    # Close database connection
+    conn.close()
+    
     return chunk
 
 def main():
@@ -29,25 +35,19 @@ def main():
     parser.add_argument("--db_file", type=str, help="Path to the SQLite database file")
     args = parser.parse_args()
 
-    # Establish connection to SQLite database
-    conn = sqlite3.connect(args.db_file)
-
     # Read CSV file in chunks
     chunksize = 10000  # Adjust as needed
     reader = pd.read_csv(args.hits_csv, delimiter=',', chunksize=chunksize)
 
     # Process chunks concurrently
     with ThreadPoolExecutor() as executor:
-        processed_chunks = executor.map(lambda chunk: fetch_descriptions(chunk, args.db_name, conn), reader)
+        processed_chunks = executor.map(lambda chunk: fetch_descriptions(chunk, args.db_name, args.db_file), reader)
 
     # Concatenate processed chunks into a single DataFrame
     df = pd.concat(processed_chunks, ignore_index=True)
 
     # Write updated DataFrame to new CSV file
     df.to_csv(args.output, index=False)
-
-    # Close database connection
-    conn.close()
 
 if __name__ == "__main__":
     main()
