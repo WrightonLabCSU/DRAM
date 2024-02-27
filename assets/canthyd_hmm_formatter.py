1f3cfdbb78528d5a6a028292bb6f9b0fef5c56d0
Reed Woyda <reedwoyda@gmail.com>
2024-02-27 12:41:09 -0700

    Added in canthyd HMM processing.

--- assets/canthyd_hmm_formatter.py
+++ assets/canthyd_hmm_formatter.py
@@ -1,28 +1,3 @@
-import pandas as pd
-import argparse
-
-def calculate_bit_score(row):
-    """Calculate bit score for each row."""
-    return row['full_score'] / row['domain_number']
-
-def calculate_rank(row):
-    """Calculate rank for each row."""
-    return row['score_rank'] if 'score_rank' in row and row['full_score'] > row['score_rank'] else row['full_score']
-
-def find_best_canthyd_hit(df):
-    """Find the best hit based on E-value and coverage."""
-    df['perc_cov'] = (df['target_end'] - df['target_start']) / df['target_length']
-    df.sort_values(by=["full_evalue", "perc_cov"], ascending=[True, False], inplace=True)
-    return df.iloc[0]
-
-def mark_best_hit_based_on_rank(df):
-    """Mark the best hit for each unique query_id based on score_rank."""
-    best_hit_idx = df["score_rank"].idxmin()
-    df_copy = df.copy()  # Make a copy to avoid modifying the original DataFrame
-    df_copy.at[best_hit_idx, "best_hit"] = True
-    return df_copy
-
-
 def main():
     parser = argparse.ArgumentParser(description="Format HMM search results.")
     parser.add_argument("--hits_csv", type=str, help="Path to the HMM search results CSV file.")
@@ -45,23 +20,22 @@ def main():
     best_hits = hits_df.groupby('query_id').apply(find_best_canthyd_hit).reset_index(drop=True)
 
     # Mark the best hit for each unique query_id based on score_rank
+    print("Marking best hit based on rank...")
     best_hits = best_hits.groupby('query_id').apply(mark_best_hit_based_on_rank).reset_index(drop=True)
 
     # Load ch_canthyd_ko file
     print("Loading ch_canthyd_ko file...")
     ch_canthyd_ko_df = pd.read_csv(args.ch_canthyd_ko, sep="\t")
-
-    print(ch_canthyd_ko_df.columns)
-
+    print(ch_canthyd_ko_df.columns)  # Print column names to verify if 'description' column is present
 
     # Merge hits_df with ch_canthyd_ko_df
+    print("Merging dataframes...")
     merged_df = pd.merge(best_hits, ch_canthyd_ko_df[['hmm_name', 'description']], left_on='target_id', right_on='hmm_name', how='left')
 
     # Extract values for canthyd_description
     merged_df['canthyd_description'] = merged_df['description']
 
     print(merged_df.head())  # Print the first few rows of the merged DataFrame
-
 
     # Add the additional columns to the output
     merged_df['start_position'] = merged_df['query_start']
@@ -78,6 +52,3 @@ def main():
     final_output_df.to_csv(args.output, index=False)
 
     print("Process completed successfully!")
-
-if __name__ == "__main__":
-    main()
