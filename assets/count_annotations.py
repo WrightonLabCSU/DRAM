ba8b28377c6a88f42ad8ed337141e3f7b7d8c622
Reed Woyda <reedwoyda@gmail.com>
2024-02-28 22:18:32 -0700

    Re-working distill summary

--- assets/count_annotations.py
+++ assets/count_annotations.py
@@ -9,28 +9,28 @@ def count_annotations(input_file, output_file):
     id_columns = [col for col in data.columns if col.endswith("_id") and col != "query_id"]
 
     # Create an empty DataFrame to store the combined data
-    target_id_data = pd.DataFrame(columns=['sample', 'target_id'])
+    gene_id_data = pd.DataFrame(columns=['sample', 'gene_id'])
 
     # Process each ID column
     for col in id_columns:
         # Explode the lists to create separate rows for each element
         exploded_data = data[['sample', col]].explode(col)
         # Rename the columns for consistency
-        exploded_data.columns = ['sample', 'target_id']
-        # Append the exploded data to the target_id_data DataFrame
-        target_id_data = pd.concat([target_id_data, exploded_data], ignore_index=True)
+        exploded_data.columns = ['sample', 'gene_id']
+        # Append the exploded data to the gene_id_data DataFrame
+        gene_id_data = pd.concat([gene_id_data, exploded_data], ignore_index=True)
 
-    # Group by "target_id" and "sample," then count the occurrences
-    table = target_id_data.groupby(['target_id', 'sample']).size().unstack(fill_value=0)
+    # Group by "gene_id" and "sample," then count the occurrences
+    table = gene_id_data.groupby(['gene_id', 'sample']).size().unstack(fill_value=0)
 
     # Save the resulting table to a TSV file
     table.to_csv(args.output_file, sep='\t')
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Count occurrences of target_ids for each sample.")
+    parser = argparse.ArgumentParser(description="Count occurrences of gene_ids for each sample.")
     parser.add_argument("input_file", help="Input file (combined_annotations.tsv)")
-    parser.add_argument("output_file", help="Output file (target_id_counts.tsv)")
+    parser.add_argument("output_file", help="Output file (gene_id_counts.tsv)")
 
     args = parser.parse_args()
 
