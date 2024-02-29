5f8a465be6ff32f1214d659fdf222637f5e5ebf8
Reed Woyda <reedwoyda@gmail.com>
2024-02-28 20:03:00 -0700

    Re-working distill summary

---
+++ assets/distill_xlsx.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import sqlite3
+import argparse
+from openpyxl import Workbook
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Generate a multi-sheet XLSX document from distill sheets and a SQLite database.')
+    parser.add_argument('--target_id_counts', type=str, help='Path to the target_id_counts.tsv file.')
+    parser.add_argument('--db_name', type=str, help='Name of the SQLite database file.')
+    parser.add_argument('--distill_sheets', nargs='+', help='List of paths to distill sheets.')
+    parser.add_argument('--rrna_file', type=str, help='Path to the rrna_sheet.tsv file.', default=None)
+    parser.add_argument('--trna_file', type=str, help='Path to the trna_sheet.tsv file.', default=None)
+    parser.add_argument('--output_file', type=str, help='Path to the output XLSX file.')
+    return parser.parse_args()
+
+def read_distill_sheets(distill_sheets):
+    sheets_data = {}
+    for sheet_path in distill_sheets:
+        df = pd.read_csv(sheet_path, sep='\t')
+        topic = df['topic_ecosystem'].unique().tolist()
+        sheets_data.update({sheet_path: {'dataframe': df, 'topics': topic}})
+    return sheets_data
+
+def query_database_for_gene_ids(db_name, gene_ids):
+    conn = sqlite3.connect(db_name)
+    placeholder= '?' # For SQLite. Adjust for other databases.
+    placeholders= ', '.join(placeholder for unused in gene_ids)
+    query = f'SELECT query_id, sample, gene_id FROM annotations WHERE gene_id IN ({placeholders})'
+    df = pd.read_sql_query(query, conn, params=gene_ids)
+    conn.close()
+    return df
+
+def compile_genome_stats(target_id_counts):
+    return pd.read_csv(target_id_counts, sep='\t')
+
+def create_topic_sheets(wb, distill_data, db_name):
+    for sheet_path, info in distill_data.items():
+        for topic in info['topics']:
+            # Query for each gene_id within this topic
+            gene_ids = info['dataframe']['gene_id'].tolist()
+            matched_annotations = query_database_for_gene_ids(db_name, gene_ids)
+            # Here, you would merge/join `matched_annotations` with `info['dataframe']` based on `gene_id`
+            # and filter by `topic`, then add to the workbook
+
+def add_trna_rrna_sheets(wb, rrna_file, trna_file):
+    if rrna_file:
+        df_rrna = pd.read_csv(rrna_file, sep='\t')
+        # Add rrna sheet to workbook
+    if trna_file:
+        df_trna = pd.read_csv(trna_file, sep='\t')
+        # Add trna sheet to workbook
+
+def main():
+    args = parse_arguments()
+
+    wb = Workbook()
+    distill_data = read_distill_sheets(args.distill_sheets)
+
+    # Compile and add genome stats sheet
+    genome_stats_df = compile_genome_stats(args.target_id_counts)
+    # Add genome stats sheet to workbook
+
+    create_topic_sheets(wb, distill_data, args.db_name)
+
+    add_trna_rrna_sheets(wb, args.rrna_file, args.trna_file)
+
+    wb.save(args.output_file)
+
+if __name__ == '__main__':
+    main()
--- modules/distill/distill.nf
+++ modules/distill/distill.nf
@@ -3,7 +3,7 @@ process DISTILL {
     input:
     path( ch_combined_annotations, stageAs: "raw-annotations.tsv" )
     path( ch_combined_distill_sheets )
-    path( ch_annotation_counts, stageAs: "target_id_counts.tsv")
+    path( ch_target_id_counts, stageAs: "target_id_counts.tsv")
     path( ch_rrna_sheet, stageAs: "rrna_sheet.tsv" )
     path( ch_combined_rrna, stageAs: "rrna_combined.tsv" )
     path( ch_trna_sheet, stageAs: "trna_sheet.tsv" )
@@ -17,9 +17,7 @@ process DISTILL {
     """
 
     python ${ch_distill_sql_script} --combined_annotations ${ch_combined_annotations} --db_name "annotations.db" 
-    
-   
+    python ${ch_distill_xlsx_script} --target_id_counts ${ch_target_id_counts} --db_name annotations.db --distill_sheets ${distill_sheets} --rrna_file ${rrna_sheet} --trna_file ${trna_sheet} --output_file "distillate.xlsx"
+
     """
 }
-
-//python ${ch_distill_xlsx_script}  --db_name annotations.db --distill_sheets ${distill_sheets} --rrna_file ${rrna_sheet} --trna_file ${trna_sheet} --output_file "distillate.xlsx"
