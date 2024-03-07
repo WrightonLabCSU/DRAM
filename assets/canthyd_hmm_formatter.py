35451934094601e10afccc5ab1a8486b20469d0e
Reed Woyda <reedwoyda@gmail.com>
2024-03-07 10:32:19 -0700

    Correcting gene start and stop locations for canthyd

--- assets/canthyd_hmm_formatter.py
+++ assets/canthyd_hmm_formatter.py
@@ -32,18 +32,22 @@ def main():
     gene_locs_df = pd.read_csv(args.gene_locs, sep='\t', header=None, names=['query_id', 'start_position', 'stop_position'])
     ch_canthyd_ko_df = pd.read_csv(args.ch_canthyd_ko, sep="\t")
 
-    if 'description' not in ch_canthyd_ko_df.columns:
-        raise ValueError("The 'description' column is missing from the ch_canthyd_ko file.")
-
     hits_df['target_id'] = hits_df['target_id'].str.replace(r'.hmm', '', regex=True)
     hits_df['bitScore'] = hits_df.apply(calculate_bit_score, axis=1)
     hits_df['score_rank'] = hits_df.apply(calculate_rank, axis=1)
 
-    merged_df = pd.merge(hits_df, ch_canthyd_ko_df, left_on='target_id', right_on='hmm_name', how='left')
+    # Perform the merge as you've described
+    merged_df = pd.merge(hits_df, ch_canthyd_ko_df[['hmm_name', 'A_rank', 'B_rank', 'description']], left_on='target_id', right_on='hmm_name', how='left')
+
+    # Merge gene locations data to update start and stop positions
     merged_df = pd.merge(merged_df, gene_locs_df, on='query_id', how='left')
 
     merged_df['canthyd_rank'] = merged_df.apply(lambda row: assign_canthyd_rank(row, row['A_rank'], row['B_rank']), axis=1)
-    merged_df['canthyd_description'] = merged_df['description']
+    if 'description' in merged_df.columns:
+        merged_df['canthyd_description'] = merged_df['description']
+    else:
+        print("Warning: 'description' column not found after merge. Check 'ch_canthyd_ko' file structure.")
+        merged_df['canthyd_description'] = 'No description available'
 
     final_output_df = merged_df[['query_id', 'start_position', 'stop_position', 'strandedness', 'target_id', 'score_rank', 'bitScore', 'canthyd_description', 'canthyd_rank']]
 
