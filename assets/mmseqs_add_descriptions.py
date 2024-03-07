270d4480cabeae47ef052540712b23cca34f9732
Reed Woyda <reedwoyda@gmail.com>
2024-03-07 12:45:37 -0700

    Correcting gene start and stop locations for canthyd

--- assets/mmseqs_add_descriptions.py
+++ assets/mmseqs_add_descriptions.py
@@ -22,20 +22,19 @@ def main(sample, db_name, descriptions_path, bit_score_threshold, gene_locs_path
         df_descriptions = pd.read_csv(descriptions_path, sep='\t')
         
         # Add database name prefix to added columns
-        df_descriptions.columns = [f"{db_name}_{col}" if col != 'blast_name' else col for col in df_descriptions.columns]
+        df_descriptions.columns = [f"{db_name}_{col}" for col in df_descriptions.columns]
         
         print("Columns in df_merged:", df_merged.columns)
         print("Columns in df_descriptions:", df_descriptions.columns)
         
-        # Merge the DataFrames on the database ID
-        df_merged = pd.merge(df_merged, df_descriptions, left_on=f"{db_name}_id", right_on=f"{db_name}_blast_name", how='left')
+        # Merge the DataFrames on the query_id and the first column of descriptions
+        df_merged = pd.merge(df_merged, df_descriptions, left_on='query_id', right_on=df_descriptions.columns[0], how='left')
         
     # Save the merged DataFrame to CSV
     output_path = f"mmseqs_out/{sample}_mmseqs_{db_name}_formatted.csv"
     df_merged.to_csv(output_path, index=False)
 
     print("Merged DataFrame saved to", output_path)
-
 
 
 if __name__ == "__main__":
