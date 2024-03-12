70d618324b451048e4c1e3533b5e3d14f3498b08
Reed Woyda <reedwoyda@gmail.com>
2024-03-12 15:34:07 -0600

    Correcting cant_hyd hmm formatter ranks

--- assets/combine_annotations.py
+++ assets/combine_annotations.py
@@ -14,17 +14,22 @@ def extract_samples_and_paths(annotation_files):
     return samples_and_paths
 
 def assign_rank(row):
-    # Adjust this function based on your database bit score columns
-    rank = 'E'  # Default rank
+    # Define default rank as 'E'
+    rank = 'E'
+
+    # Check for conditions that would assign a higher rank
     if 'kegg_bitScore' in row and row['kegg_bitScore'] > 350:
         rank = 'A'
     elif 'uniref_bitScore' in row and row['uniref_bitScore'] > 350:
         rank = 'B'
-    elif ('kegg_bitScore' in row and row['kegg_bitScore'] > 60) or ('uniref_bitScore' in row and row['uniref_bitScore'] > 60):
+    elif (('kegg_bitScore' in row and row['kegg_bitScore'] > 60) or 
+        ('uniref_bitScore' in row and row['uniref_bitScore'] > 60)):
         rank = 'C'
-    elif any(db in row for db in ['pfam_id', 'dbcan_id', 'merops_id']) and not (row.get('kegg_bitScore', 0) > 60 or row.get('uniref_bitScore', 0) > 60):
+    elif any(row.get(db + '_bitScore', 0) > 60 for db in ['pfam', 'dbcan', 'merops']) and \
+        all(row.get(db + '_bitScore', 0) <= 60 for db in ['kegg', 'uniref']):
         rank = 'D'
-    # Note: Rank 'E' is already the default
+    # If there are any significant hits to VOGDB or no hits to any database, it remains 'E'
+    
     return rank
 
 def organize_columns(df):
