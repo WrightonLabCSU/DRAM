7503676e52bcd5cda97a1fb6ddb7d41ccd744c8b
Reed Woyda <reedwoyda@gmail.com>
2024-03-13 15:04:19 -0600

    Implementing GBK generation

--- assets/generate_gff_genbank.py
+++ assets/generate_gff_genbank.py
@@ -127,26 +127,20 @@ def generate_gbk(samples_annotations, database_list, samples_and_paths):
                 # Ensure the molecule_type is set for each SeqRecord
                 seq_record.annotations["molecule_type"] = "DNA"
 
-                # Set additional metadata for GBK file here
+                # Hard-code the source as "DRAM2"
+                seq_record.annotations["source"] = "DRAM2"
+
+                # Set taxonomy information under ORGANISM
                 if annotations:
                     metadata = annotations[0]  # Assuming shared metadata across each sample's annotations
                     taxonomy_info = metadata.get('taxonomy', 'Not Available')
-                    completeness_info = str(metadata.get('Completeness', 'Not Available'))
-                    contamination_info = str(metadata.get('Contamination', 'Not Available'))
-                    
-                    seq_record.annotations["source"] = taxonomy_info
                     seq_record.annotations["organism"] = taxonomy_info
-                    seq_record.annotations["note"] = f"Completeness: {completeness_info}; Contamination: {contamination_info}"
+                
+                # Adding note for Completeness and Contamination
+                completeness_info = str(metadata.get('Completeness', 'Not Available'))
+                contamination_info = str(metadata.get('Contamination', 'Not Available'))
+                seq_record.annotations["note"] = f"Completeness: {completeness_info}; Contamination: {contamination_info}"
 
-                for annotation in annotations:
-                    query_id = annotation['query_id']
-                    if query_id in sequences:
-                        sequence = sequences[query_id]
-                        feature_location = FeatureLocation(start=int(annotation['start_position']) - 1, end=int(annotation['stop_position']), strand=1 if annotation['strandedness'] == '+1' else -1)
-                        qualifiers = format_qualifiers(annotation, database_list)
-                        feature = SeqFeature(feature_location, type="gene", qualifiers=qualifiers)
-                        seq_record.features.append(feature)
-                        
                 for annotation in annotations:
                     query_id = annotation['query_id']
                     if query_id in sequences:
@@ -164,6 +158,7 @@ def generate_gbk(samples_annotations, database_list, samples_and_paths):
                 print(f"File does not exist: {fna_file_path}")
         else:
             print(f"No .fna file path found for sample {sample}")
+
 
 def main():
     args = parse_arguments()
