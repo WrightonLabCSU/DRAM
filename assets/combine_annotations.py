28688be411919b0762adf93d5b8d438e5a84c98c
Reed Woyda <reedwoyda@gmail.com>
2024-03-15 10:53:17 -0600

    Modifying rRNA_Scan and rRNA_collect to set the output file contents = NULL if nothing is generated. This should agree well with the current distill implementation.

--- assets/combine_annotations.py
+++ assets/combine_annotations.py
@@ -50,25 +50,27 @@ def combine_annotations(annotation_files, output_file, threads):
     combined_data = combined_data.drop_duplicates(subset=['query_id', 'start_position', 'stop_position'])
     combined_data = convert_bit_scores_to_numeric(combined_data)
     combined_data['rank'] = combined_data.apply(assign_rank, axis=1)
+    
+    # Extract base query_id without the trailing count
+    combined_data['base_query_id'] = combined_data['query_id'].str.rsplit('_', 1).str[0]
+    
     combined_data = organize_columns(combined_data)
-    # Sort data by 'sample', 'query_id', and 'start_position' to correctly assign 'gene_number'
-    combined_data.sort_values(by=['sample', 'query_id', 'start_position'], ascending=True, inplace=True)
+    # Sort data by 'sample', 'base_query_id', and 'start_position' to correctly assign 'gene_number'
+    combined_data.sort_values(by=['sample', 'base_query_id', 'start_position'], ascending=True, inplace=True)
 
-    # Extract unique gene identifier from 'query_id'
-    combined_data['gene_identifier'] = combined_data['query_id'].apply(lambda x: "_".join(x.split('_')[:-1]))
-    # Group by 'sample' and 'gene_identifier' and enumerate entries based on their sorted order
-    combined_data['gene_number'] = combined_data.groupby(['sample', 'query_id']).cumcount() + 1
-    # Drop the temporary 'gene_identifier' column
-    combined_data.drop(columns=['gene_identifier'], inplace=True)
-
+    # Group by 'sample' and 'base_query_id' and enumerate entries based on their sorted order
+    combined_data['gene_number'] = combined_data.groupby(['sample', 'base_query_id']).cumcount() + 1
+    
     # Ensure "gene_number" comes after "rank"
     col_order = combined_data.columns.tolist()
     rank_index = col_order.index('rank')
     col_order.insert(rank_index + 1, col_order.pop(col_order.index('gene_number')))
     combined_data = combined_data[col_order]
 
+    combined_data.drop(columns=['base_query_id'], inplace=True)  # Drop the temporary 'base_query_id' column
+
     combined_data.to_csv(output_file, index=False, sep='\t')
-    logging.info(f"Combined annotations saved to {output_file}, with ranks assigned, sorted by 'sample', 'query_id', and 'start_position', and 'gene_number' correctly positioned.")
+    logging.info(f"Combined annotations saved to {output_file}, with ranks assigned, sorted by 'sample', 'base_query_id', and 'start_position', and 'gene_number' correctly positioned.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Combine annotation files with ranks and avoid duplicating specific columns.")
