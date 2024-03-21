db6b41e56855ffb26e274e6be1dd2bebef081cf9
Reed Woyda <reedwoyda@gmail.com>
2024-03-21 15:39:04 -0600

    Modifies trna collect process to correctly detect empy input files.

--- assets/combine_annotations.py
+++ assets/combine_annotations.py
@@ -34,11 +34,30 @@ def convert_bit_scores_to_numeric(df):
     return df
 
 def organize_columns(df):
-    base_columns = ['query_id', 'sample', 'start_position', 'stop_position', 'strandedness', 'rank']
+    # Base columns to always appear first
+    base_columns = ['query_id', 'sample', 'start_position', 'stop_position', 'strandedness', 'rank', 'gene_number']
+    
+    # Extract KEGG columns and ensure they are listed first if present
     kegg_columns = [col for col in df.columns if col.startswith('kegg_')]
-    other_columns = [col for col in df.columns if col not in base_columns and col not in kegg_columns]
-    final_columns_order = base_columns + kegg_columns + other_columns
+    kegg_columns_sorted = sorted(kegg_columns, key=lambda x: (x != 'kegg_id', x))
+    
+    # Identify and organize other database columns
+    db_columns = [col for col in df.columns if col not in base_columns and col not in kegg_columns]
+    
+    # Identify unique database prefixes
+    db_prefixes = set(col.split('_')[0] for col in db_columns)
+    
+    # Sort and organize columns for each database, ensuring <db_name>_id is the first column
+    other_db_columns_sorted = []
+    for prefix in sorted(db_prefixes):
+        prefixed_columns = [col for col in db_columns if col.startswith(prefix + '_')]
+        prefixed_columns_sorted = sorted(prefixed_columns, key=lambda x: (x != f"{prefix}_id", x))
+        other_db_columns_sorted.extend(prefixed_columns_sorted)
+    
+    # Final order of columns
+    final_columns_order = base_columns + kegg_columns_sorted + other_db_columns_sorted
     return df[final_columns_order]
+
 
 def combine_annotations(annotation_files, output_file, threads):
     samples_and_paths = [(annotation_files[i].strip('[], '), annotation_files[i + 1].strip('[], ')) for i in range(0, len(annotation_files), 2)]
@@ -47,41 +66,30 @@ def combine_annotations(annotation_files, output_file, threads):
         futures = [executor.submit(read_and_preprocess, sample, path) for sample, path in samples_and_paths]
         data_frames = [future.result() for future in as_completed(futures)]
     
-    # Combine data frames while checking for special columns to ensure they are not duplicated
+    # Initialize an empty DataFrame for combined data
     combined_data = pd.DataFrame()
-    special_columns = ['Completeness', 'Contamination', 'taxonomy']
+    # Track if special columns have been added already
+    special_columns_added = {'Completeness': False, 'Contamination': False, 'taxonomy': False}
+    
     for df in data_frames:
-        # Rename duplicates in the current df before concatenation
-        for col in special_columns:
-            if col in combined_data.columns and col in df.columns:
-                df.drop(columns=[col], inplace=True)  # Drop or rename as needed
+        # Check and drop special columns if they've been added already
+        for col in ['Completeness', 'Contamination', 'taxonomy']:
+            if col in df.columns and special_columns_added[col]:
+                df.drop(columns=[col], inplace=True)
+            elif col in df.columns:
+                special_columns_added[col] = True
+                
         combined_data = pd.concat([combined_data, df], ignore_index=True)
     
-    combined_data = combined_data.drop_duplicates(subset=['query_id', 'start_position', 'stop_position'])
+    combined_data = combined_data.drop_duplicates(subset=['query_id', 'start_position', 'stop_position', 'sample'])
     combined_data = convert_bit_scores_to_numeric(combined_data)
     combined_data['rank'] = combined_data.apply(assign_rank, axis=1)
     
-    # Create a new identifier for sorting that combines the non-numeric part of query_id and its numeric suffix
-    combined_data['sorting_id'] = combined_data['query_id'].str.extract(r'(.+)_([0-9]+)$', expand=True).apply(lambda x: f"{x[0]}_{int(x[1]):05d}", axis=1)
-    
     combined_data = organize_columns(combined_data)
-    # Sort data by 'sample' and 'sorting_id' to correctly assign 'gene_number'
-    combined_data.sort_values(by=['sample', 'sorting_id'], ascending=True, inplace=True)
-
-    # Group by 'sample' and original 'query_id' base (without the numeric part) and enumerate entries based on their sorted order
-    combined_data['base_query_id'] = combined_data['query_id'].apply(lambda x: "_".join(x.split('_')[:-1]))
-    combined_data['gene_number'] = combined_data.groupby(['sample', 'base_query_id']).cumcount() + 1
+    combined_data['gene_number'] = combined_data.groupby(['sample', 'query_id']).cumcount() + 1
     
-    # Ensure "gene_number" comes after "rank"
-    col_order = combined_data.columns.tolist()
-    rank_index = col_order.index('rank')
-    col_order.insert(rank_index + 1, col_order.pop(col_order.index('gene_number')))
-    combined_data = combined_data[col_order]
-
-    combined_data.drop(columns=['sorting_id', 'base_query_id'], inplace=True)  # Drop the temporary 'sorting_id' and 'base_query_id' columns
-
     combined_data.to_csv(output_file, index=False, sep='\t')
-    logging.info(f"Combined annotations saved to {output_file}, with ranks assigned, sorted by 'sample' and 'query_id' numerically, and 'gene_number' correctly positioned.")
+    logging.info(f"Combined annotations saved to {output_file}.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Combine annotation files with ranks and avoid duplicating specific columns.")
