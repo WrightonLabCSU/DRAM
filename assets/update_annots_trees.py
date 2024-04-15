4ad766a34639682b55146273d539c4e83bbb3c49
Reed Woyda <reedwoyda@gmail.com>
2024-04-15 09:52:39 -0600

    Added TREES process to singularity_slurm.config ONLY.

--- assets/update_annots_trees.py
+++ assets/update_annots_trees.py
@@ -35,31 +35,22 @@ def find_label_for_edge(tree, edge_number):
             return clade.name
     return "No matching label found"
 
-def update_annotations(annotations_file, placements, tree, tree_mapping):
-    annotations_df = pd.read_csv(annotations_file, sep='\t')
-    annotations_df['tree-verified'] = None
-    for index, row in annotations_df.iterrows():
-        gene_id = row['query_id']
-        if gene_id in placements:
-            edge, lwr, likelihood = placements[gene_id]
-            closest_label = find_label_for_edge(tree, edge)
-            mapped_label = tree_mapping.get(closest_label, "No matching tree mapping found")
-            annotations_df.at[index, 'tree-verified'] = mapped_label
-    return annotations_df
+def print_closest_tip_labels(tree, placements):
+    for gene_id, (edge, _, _) in placements.items():
+        closest_label = find_label_for_edge(tree, edge)
+        print(f"Gene: {gene_id}, Closest Tip Label: {closest_label}")
 
-def main(jplace_file, mapping_file, annotations_file, output_file):
+def main(jplace_file, mapping_file):
     output_dir = './output'
     os.makedirs(output_dir, exist_ok=True)
-    tree_path, edpl_path = run_guppy(jplace_file, output_dir)
+    tree_path, _ = run_guppy(jplace_file, output_dir)
     tree = load_phylogenetic_tree(tree_path)
     tree_mapping = load_tree_mapping(mapping_file)
     placements = extract_closest_matches(jplace_file)
-    updated_annotations = update_annotations(annotations_file, placements, tree, tree_mapping)
-    updated_annotations.to_csv(output_file, sep='\t', index=False)
-    print("Updated annotations written to file.")
+    print_closest_tip_labels(tree, placements)
 
 if __name__ == '__main__':
-    if len(sys.argv) < 5:
-        print("Usage: python script.py <jplace_file> <mapping_file> <annotations_file> <output_file>")
+    if len(sys.argv) < 3:
+        print("Usage: python script.py <jplace_file> <mapping_file>")
         sys.exit(1)
-    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    main(sys.argv[1], sys.argv[2])
