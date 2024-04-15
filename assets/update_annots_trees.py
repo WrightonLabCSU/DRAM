22d22b3cf2ad081e051151d931f5bab14008e416
Reed Woyda <reedwoyda@gmail.com>
2024-04-15 10:15:38 -0600

    Added TREES process to singularity_slurm.config ONLY.

--- assets/update_annots_trees.py
+++ assets/update_annots_trees.py
@@ -1,6 +1,7 @@
 import json
 import subprocess
 import os
+from Bio import Phylo
 
 def run_guppy(jplace_file, output_dir):
     subprocess.run(['guppy', 'tog', jplace_file, '-o', f"{output_dir}/tree_with_placements.newick"], check=True)
@@ -19,16 +20,30 @@ def extract_placements(jplace_file):
             placements[gene_name] = edge_number
     return placements
 
-def print_closest_tip_labels(placements):
+def find_closest_tip_labels(placements, tree_path):
+    tree = Phylo.read(tree_path, 'newick')
+    closest_tip_labels = {}
     for gene_id, edge_number in placements.items():
-        print(f"Sample: {gene_id}, Closest Edge Number: {edge_number}")
+        closest_tip_labels[gene_id] = find_label_for_edge(tree, edge_number)
+    return closest_tip_labels
+
+def find_label_for_edge(tree, edge_number):
+    for clade in tree.find_clades():
+        if str(clade.comment) == str(edge_number):
+            return clade.name
+    return "No matching label found"
+
+def print_closest_tip_labels(closest_tip_labels):
+    for gene_id, tip_label in closest_tip_labels.items():
+        print(f"Sample: {gene_id}, Closest Tip Label: {tip_label}")
 
 def main(jplace_file):
     output_dir = './output'
     os.makedirs(output_dir, exist_ok=True)
-    _, _ = run_guppy(jplace_file, output_dir)
+    tree_path, _ = run_guppy(jplace_file, output_dir)
     placements = extract_placements(jplace_file)
-    print_closest_tip_labels(placements)
+    closest_tip_labels = find_closest_tip_labels(placements, tree_path)
+    print_closest_tip_labels(closest_tip_labels)
 
 if __name__ == '__main__':
     import sys
