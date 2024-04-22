63ced99eb34b98f5fbe4f56cbf7739f15142cf7a
Reed Woyda <reedwoyda@gmail.com>
2024-04-22 10:56:16 -0600

    Generalizing TREES process to run a set of trees by default unless the user uses the option: --no_trees

--- assets/parse_annotations.py
+++ assets/parse_annotations.py
@@ -1,26 +1,25 @@
 import pandas as pd
 import sys
 
-def extract_query_ids(tsv_path, ko_list):
+def extract_query_ids(tsv_path, ko_terms):
     df = pd.read_csv(tsv_path, sep='\t')
     
-    # Split the ko_list string into terms, separating on semicolons
-    ko_terms = ko_list.split(';')
+    # Split KO terms on newline to handle multiple entries correctly
+    ko_terms = ko_terms.strip().split('\n')
+    print(f"Searching for KOs: {ko_terms}")
+
+    # Define columns to search for KO terms, excluding 'query_id'
+    search_columns = [col for col in df.columns if (col.endswith('_id') or col.endswith('_description') or col.endswith('_EC')) and col != 'query_id']
     
-    # Identify columns for gene IDs, EC numbers, and descriptions
-    id_columns = [col for col in df.columns if col.endswith('_id') and col != 'query_id']
-    ec_columns = [col for col in df.columns if col.endswith('_EC')]
-    desc_columns = [col for col in df.columns if col.endswith('_description')]
-    
-    # Create filters for ID, EC, and description columns
-    id_mask = df[id_columns].apply(lambda x: x.isin(ko_terms)).any(axis=1)
-    ec_mask = df[ec_columns].apply(lambda x: x.isin(ko_terms)).any(axis=1)
-    desc_mask = df[desc_columns].apply(lambda x: x.astype(str).apply(lambda y: any(term in y for term in ko_terms))).any(axis=1)
-    
-    # Combine all masks to filter the DataFrame
-    combined_mask = id_mask | ec_mask | desc_mask
-    filtered_df = df[combined_mask]
-    
+    # Debugging: Output the columns being searched
+    print(f"Searching in columns: {search_columns}")
+
+    # Create a filter mask for any row containing any of the KO terms in the specified columns
+    mask = df[search_columns].apply(lambda x: x.astype(str).str.contains('|'.join(ko_terms), case=False, na=False)).any(axis=1)
+    filtered_df = df[mask]
+
+    print(f"Found {len(filtered_df)} matching entries.")
+
     return filtered_df[['sample', 'query_id']]
 
 def main():
@@ -28,7 +27,10 @@ def main():
         print("Usage: python parse_annotations.py <tsv_path> <ko_list> <output_file>")
         sys.exit(1)
 
-    tsv_path, ko_list, output_file = sys.argv[1:]
+    tsv_path, ko_list_path, output_file = sys.argv[1:]
+    with open(ko_list_path, 'r') as f:
+        ko_list = f.read()
+
     results_df = extract_query_ids(tsv_path, ko_list)
 
     # Write results to output file
