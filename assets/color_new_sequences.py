c3c779bb9da1f75ef6a96295087387f41ded1ba0
Reed Woyda <reedwoyda@gmail.com>
2024-05-13 11:46:31 -0600

    Reworking dmso tree package

--- assets/color_new_sequences.py
+++ assets/color_new_sequences.py
@@ -1,31 +1,44 @@
-import xml.etree.ElementTree as ET
 import sys
+import xml.etree.ElementTree as ET
 
-def color_new_sequences(xml_path, new_sequences, output_path):
+def color_new_sequences(xml_path, query_ids_path, output_path):
+    # Load the list of query IDs to be colored
+    with open(query_ids_path, 'r') as f:
+        query_ids = set(line.strip().split('\t')[1] for line in f.readlines())
+
+    # Parse the XML tree
     tree = ET.parse(xml_path)
     root = tree.getroot()
 
-    for node in root.findall('.//clade'):
-        name_elem = node.find('name')
-        if name_elem is not None and name_elem.text in new_sequences:
-            branch_color = ET.SubElement(node, 'branch_color')
-            branch_color.set('r', '255')
-            branch_color.set('g', '0')
-            branch_color.set('b', '0')
-            branch_color.set('a', '1.0')
+    # Function to recursively find all nodes and color the new sequences
+    def color_nodes(element):
+        for clade in element.findall('.//{http://www.phyloxml.org}clade'):
+            name = clade.find('{http://www.phyloxml.org}name')
+            if name is not None and name.text in query_ids:
+                color = ET.Element('{http://www.phyloxml.org}color')
+                red = ET.Element('{http://www.phyloxml.org}red')
+                green = ET.Element('{http://www.phyloxml.org}green')
+                blue = ET.Element('{http://www.phyloxml.org}blue')
+                red.text = '255'
+                green.text = '0'
+                blue.text = '0'
+                color.append(red)
+                color.append(green)
+                color.append(blue)
+                clade.append(color)
+            # Recursively color nested clades
+            color_nodes(clade)
 
+    # Start coloring from the root element
+    color_nodes(root)
+
+    # Write the modified tree to the output file
     tree.write(output_path)
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: python color_new_sequences.py <xml_path> <new_sequences_file> <output_path>")
+        print("Usage: python color_new_sequences.py <input_xml> <query_ids> <output_xml>")
         sys.exit(1)
 
-    xml_path = sys.argv[1]
-    new_sequences_file = sys.argv[2]
-    output_path = sys.argv[3]
-
-    with open(new_sequences_file) as f:
-        new_sequences = [line.strip() for line in f]
-
-    color_new_sequences(xml_path, new_sequences, output_path)
+    input_xml, query_ids, output_xml = sys.argv[1:]
+    color_new_sequences(input_xml, query_ids, output_xml)
--- modules/trees/trees.nf
+++ modules/trees/trees.nf
@@ -12,8 +12,7 @@ process TREES {
 
     output:
     path("updated-annotations.tsv"), emit: updated_annotations, optional: true
-    path("aligned_sequences.jplace"), emit: tree_placements, optional: true
-    path("aligned_sequences.xml"), emit: tree_visualization, optional: true
+    path("aligned_sequences_colored.xml"), emit: tree_visualization, optional: true
 
     script:
     """        
