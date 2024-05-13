59e9d55e50b6ad07fd783fa3f08caf456a1fe6e5
Reed Woyda <reedwoyda@gmail.com>
2024-05-13 11:51:25 -0600

    Reworking dmso tree package

--- assets/color_new_sequences.py
+++ assets/color_new_sequences.py
@@ -10,15 +10,18 @@ def color_new_sequences(xml_path, query_ids_path, output_path):
     tree = ET.parse(xml_path)
     root = tree.getroot()
 
+    # Define the phyloXML namespace
+    ns = {'phy': 'http://www.phyloxml.org'}
+
     # Function to recursively find all nodes and color the new sequences
     def color_nodes(element):
-        for clade in element.findall('.//{http://www.phyloxml.org}clade'):
-            name = clade.find('{http://www.phyloxml.org}name')
+        for clade in element.findall('.//phy:clade', ns):
+            name = clade.find('phy:name', ns)
             if name is not None and name.text in query_ids:
-                color = ET.Element('{http://www.phyloxml.org}color')
-                red = ET.Element('{http://www.phyloxml.org}red')
-                green = ET.Element('{http://www.phyloxml.org}green')
-                blue = ET.Element('{http://www.phyloxml.org}blue')
+                color = ET.Element('phy:color', ns)
+                red = ET.Element('phy:red', ns)
+                green = ET.Element('phy:green', ns)
+                blue = ET.Element('phy:blue', ns)
                 red.text = '255'
                 green.text = '0'
                 blue.text = '0'
@@ -32,8 +35,11 @@ def color_new_sequences(xml_path, query_ids_path, output_path):
     # Start coloring from the root element
     color_nodes(root)
 
+    # Register namespace
+    ET.register_namespace('', "http://www.phyloxml.org")
+
     # Write the modified tree to the output file
-    tree.write(output_path)
+    tree.write(output_path, encoding='utf-8', xml_declaration=True)
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
