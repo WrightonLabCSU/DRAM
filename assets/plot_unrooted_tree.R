dec66ef2872ae3dbf11f59d6b302caafa7abc0bf
Reed Woyda <reedwoyda@gmail.com>
2024-05-14 15:03:42 -0600

    Reworking dmso tree package

--- assets/plot_unrooted_tree.R
+++ assets/plot_unrooted_tree.R
@@ -12,8 +12,16 @@ tree <- read.tree(newick_file)
 # Read the labels to be colored
 labels_to_color <- readLines(labels_file)
 
+# Print the labels to be colored for debugging
+cat("Labels to be colored:\n")
+print(labels_to_color)
+
 # Ensure labels to color are present in the tree
 valid_labels <- tree$tip.label %in% labels_to_color
+
+# Print the valid labels for debugging
+cat("Valid labels found in the tree:\n")
+print(tree$tip.label[valid_labels])
 
 # Plot the unrooted tree
 png(output_png, width = 1024, height = 1024)
