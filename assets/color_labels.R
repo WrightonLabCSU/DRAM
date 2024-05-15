84397447a4009a1d772edf692f8cd34ce9d470c5
Reed Woyda <reedwoyda@gmail.com>
2024-05-15 13:03:58 -0600

    Reworking dmso tree package

--- assets/color_labels.R
+++ assets/color_labels.R
@@ -10,7 +10,7 @@ tree <- read.tree(newick_file)
 
 # Read the labels to be colored
 labels_to_color_raw <- readLines(labels_file)
-labels_to_color <- sub("\t", " ", labels_to_color_raw)
+labels_to_color <- gsub("\t", " ", labels_to_color_raw)
 
 # Define the labels present in the tree
 valid_labels <- tree$tip.label
@@ -18,16 +18,23 @@ valid_labels <- tree$tip.label
 # Identify the indices of the tips to color
 tips_to_color <- which(valid_labels %in% labels_to_color)
 
+# Check if there are tips to color
+if (length(tips_to_color) == 0) {
+  stop("No matching tips found in the tree for coloring.")
+}
+
 # Plot the tree with colored tips
 pdf(output_pdf, width=20, height=20)
 plot(tree, type = "unrooted", show.tip.label = FALSE)
 
 # Color the tips
 tiplabels(pch = 16, col = "red", tip = tips_to_color)
-tiplabels(text = valid_labels[tips_to_color], tip = tips_to_color, col = "red", adj = 0, cex = 0.5, offset = 0.5, frame = "none", srt = 45)
+tiplabels(text = valid_labels[tips_to_color], tip = tips_to_color, col = "red", adj = 0, cex = 0.5)
 
 # Add non-colored tip labels
 non_colored_tips <- setdiff(seq_along(valid_labels), tips_to_color)
-tiplabels(text = valid_labels[non_colored_tips], tip = non_colored_tips, col = "black", adj = 0, cex = 0.5, offset = 0.5, frame = "none", srt = 45)
+if (length(non_colored_tips) > 0) {
+  tiplabels(text = valid_labels[non_colored_tips], tip = non_colored_tips, col = "black", adj = 0, cex = 0.5)
+}
 
 dev.off()
