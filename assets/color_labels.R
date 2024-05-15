24c6cdb7983d68213e0be804c1cbf322d83c1631
Reed Woyda <reedwoyda@gmail.com>
2024-05-15 12:36:57 -0600

    Reworking dmso tree package

--- assets/color_labels.R
+++ assets/color_labels.R
@@ -20,20 +20,17 @@ label_colors <- rep("black", length(tree$tip.label))
 # Color the specified labels in red
 label_colors[tree$tip.label %in% labels_to_color] <- "red"
 
-# Calculate the coordinates for the labels
-tip_coords <- ape::get("last_plot.phylo", envir = .PlotPhyloEnv)$xx[1:length(tree$tip.label),]
-angles <- ape::get("last_plot.phylo", envir = .PlotPhyloEnv)$theta[1:length(tree$tip.label)]
-
-# Plot the tree
+# Plot the tree and extract coordinates
 pdf(output_pdf, width = 10, height = 10)
 plot(tree, type = "unrooted", show.tip.label = FALSE)
+last_plot <- get("last_plot.phylo", envir = .PlotPhyloEnv)
 
 # Add labels with colors and angles
 for (i in 1:length(tree$tip.label)) {
   label <- tree$tip.label[i]
   color <- label_colors[i]
-  angle <- ifelse(angles[i] > 180, angles[i] - 180, angles[i])
-  text(tip_coords[i, 1], tip_coords[i, 2], labels = label, pos = 4, srt = angle, cex = 0.6, col = color)
+  angle <- ifelse(last_plot$theta[i] > 180, last_plot$theta[i] - 180, last_plot$theta[i])
+  text(last_plot$xx[i], last_plot$yy[i], labels = label, pos = 4, srt = angle, cex = 0.6, col = color)
 }
 
 dev.off()
