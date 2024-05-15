3e443413a60b3f16488eaed3a59c98c78f546ac7
Reed Woyda <reedwoyda@gmail.com>
2024-05-15 12:58:07 -0600

    Reworking dmso tree package

--- assets/color_labels.R
+++ assets/color_labels.R
@@ -1,36 +1,37 @@
 library(ape)
 
+# Define the input files
 args <- commandArgs(trailingOnly = TRUE)
 newick_file <- args[1]
 labels_file <- args[2]
 output_pdf <- args[3]
 
+# Read the Newick tree
 tree <- read.tree(newick_file)
+
+# Read the labels to be colored
 labels_to_color_raw <- readLines(labels_file)
+labels_to_color <- sub(".*\t", "", labels_to_color_raw)
 
-labels_to_color <- gsub("\t", " ", labels_to_color_raw)
-labels_to_color <- trimws(labels_to_color)
+# Ensure unique labels
+labels_to_color <- unique(labels_to_color)
 
-tip_colors <- rep("black", length(tree$tip.label))
-tip_labels <- tree$tip.label
+# Open PDF device with increased size
+pdf(output_pdf, width = 15, height = 15)
+
+# Plot the tree without labels
+plot(tree, type = "unrooted", show.tip.label = FALSE)
+
+# Add colored tip labels without duplicating
+tiplabels(pch = 19, col = "black", adj = c(1, 1))
 
 for (i in seq_along(tree$tip.label)) {
-  for (label in labels_to_color) {
-    if (grepl(label, tree$tip.label[i], fixed = TRUE)) {
-      tip_colors[i] <- "red"
-      tip_labels[i] <- tree$tip.label[i]
-      break
-    }
+  if (tree$tip.label[i] %in% labels_to_color) {
+    tiplabels(tree$tip.label[i], i, col = "red", adj = c(1, 1), cex = 0.6, font = 2)
+  } else {
+    tiplabels(tree$tip.label[i], i, col = "black", adj = c(1, 1), cex = 0.6, font = 1)
   }
 }
 
-pdf(output_pdf, width = 16, height = 16)
-plot(tree, type = "unrooted", show.tip.label = FALSE)
-
-offset <- 0.02
-cex <- 0.5
-for (i in seq_along(tree$tip.label)) {
-  tiplabels(tip_labels[i], adj = c(0.5, 0.5), frame = "none", col = tip_colors[i], cex = cex, offset = offset)
-}
-
+# Close the PDF device
 dev.off()
