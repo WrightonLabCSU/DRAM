0eadc8620d4a4d67dbf40a8e0f0b5508d5651896
Reed Woyda <reedwoyda@gmail.com>
2024-05-15 08:53:26 -0600

    Reworking dmso tree package

--- assets/color_labels.R
+++ assets/color_labels.R
@@ -18,12 +18,16 @@ labels_to_color <- gsub("\t", "", labels_to_color_raw)
 label_data <- data.frame(label = tree$tip.label, color = "black", stringsAsFactors = FALSE)
 label_data$color[label_data$label %in% labels_to_color] <- "red"
 
+# Ensure no duplicates in the labels
+label_data <- label_data[!duplicated(label_data$label),]
+
 # Create a ggtree plot
 p <- ggtree(tree, layout = "unrooted") +
   geom_tiplab(aes(label = label, color = color), size = 2.5, show.legend = FALSE) +
   scale_color_manual(values = c("black" = "black", "red" = "red")) +
   theme_tree2() +
-  theme(legend.position = "none")
+  theme(legend.position = "none") +
+  geom_text_repel(aes(label = label, color = color), size = 2.5, show.legend = FALSE, nudge_x = 0.5, nudge_y = 0.5)
 
 # Save the plot to a PDF file
 ggsave(output_pdf, plot = p, width = 20, height = 20)
