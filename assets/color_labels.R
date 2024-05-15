d9266e3a2b9d9b114303398c2485e54154721db4
Reed Woyda <reedwoyda@gmail.com>
2024-05-15 13:02:05 -0600

    Reworking dmso tree package

--- assets/color_labels.R
+++ assets/color_labels.R
@@ -1,37 +1,33 @@
 library(ape)
 
 # Define the input files
-args <- commandArgs(trailingOnly = TRUE)
-newick_file <- args[1]
-labels_file <- args[2]
-output_pdf <- args[3]
+newick_file <- commandArgs(trailingOnly = TRUE)[1]
+labels_file <- commandArgs(trailingOnly = TRUE)[2]
+output_pdf <- commandArgs(trailingOnly = TRUE)[3]
 
 # Read the Newick tree
 tree <- read.tree(newick_file)
 
 # Read the labels to be colored
 labels_to_color_raw <- readLines(labels_file)
-labels_to_color <- sub(".*\t", "", labels_to_color_raw)
+labels_to_color <- sub("\t", " ", labels_to_color_raw)
 
-# Ensure unique labels
-labels_to_color <- unique(labels_to_color)
+# Define the labels present in the tree
+valid_labels <- tree$tip.label
 
-# Open PDF device with increased size
-pdf(output_pdf, width = 15, height = 15)
+# Identify the indices of the tips to color
+tips_to_color <- which(valid_labels %in% labels_to_color)
 
-# Plot the tree without labels
+# Plot the tree with colored tips
+pdf(output_pdf, width=20, height=20)
 plot(tree, type = "unrooted", show.tip.label = FALSE)
 
-# Add colored tip labels without duplicating
-tiplabels(pch = 19, col = "black", adj = c(1, 1))
+# Color the tips
+tiplabels(pch = 16, col = "red", tip = tips_to_color)
+tiplabels(text = valid_labels[tips_to_color], tip = tips_to_color, col = "red", adj = 0, cex = 0.5, offset = 0.5, frame = "none", srt = 45)
 
-for (i in seq_along(tree$tip.label)) {
-  if (tree$tip.label[i] %in% labels_to_color) {
-    tiplabels(tree$tip.label[i], i, col = "red", adj = c(1, 1), cex = 0.6, font = 2)
-  } else {
-    tiplabels(tree$tip.label[i], i, col = "black", adj = c(1, 1), cex = 0.6, font = 1)
-  }
-}
+# Add non-colored tip labels
+non_colored_tips <- setdiff(seq_along(valid_labels), tips_to_color)
+tiplabels(text = valid_labels[non_colored_tips], tip = non_colored_tips, col = "black", adj = 0, cex = 0.5, offset = 0.5, frame = "none", srt = 45)
 
-# Close the PDF device
 dev.off()
