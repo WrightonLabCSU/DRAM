588850465ac661dc7a7a9e5693b7b140077e0752
Reed Woyda <reedwoyda@gmail.com>
2024-05-15 10:58:11 -0600

    Reworking dmso tree package

--- assets/color_labels.R
+++ assets/color_labels.R
@@ -10,27 +10,25 @@ output_pdf <- args[3]
 tree <- read.tree(newick_file)
 
 # Read the labels to be colored
-labels_to_color <- readLines(labels_file)
+labels_to_color_raw <- readLines(labels_file)
+labels_to_color <- sapply(labels_to_color_raw, function(x) strsplit(x, "\t")[[1]][2])
 
-# Remove tab spaces from labels to color
-labels_to_color <- sub("\t", " ", labels_to_color)
-
-# Set up colors
-tip_colors <- rep("black", length(tree$tip.label))
-names(tip_colors) <- tree$tip.label
-
-# Color the tips that match the labels
-for (label in labels_to_color) {
-  if (label %in% names(tip_colors)) {
-    tip_colors[label] <- "red"
-  }
+# Function to color the labels
+color_labels <- function(tree, labels, color) {
+  is_tip <- tree$edge[, 2] <= length(tree$tip.label)
+  ordered_tips <- tree$tip.label[tree$edge[is_tip, 2]]
+  col <- rep("black", length(ordered_tips))
+  col[ordered_tips %in% labels] <- color
+  return(col)
 }
 
-# Increase the plot size
-pdf(output_pdf, width = 15, height = 15)
-plot(tree, type = "unrooted", show.tip.label = FALSE, cex = 0.8)
+# Apply coloring to the labels
+tip_colors <- color_labels(tree, labels_to_color, "red")
 
-# Add colored labels
-tiplabels(tree$tip.label, tip = 1:length(tree$tip.label), bg = tip_colors, frame = "none", col = tip_colors, cex = 0.8, adj = 1)
-
+# Plot the tree with colored labels
+pdf(output_pdf, width = 16, height = 16)
+plot(tree, type = "unrooted", show.tip.label = TRUE, tip.color = tip_colors, cex = 0.5)
+tiplabels(tree$tip.label[tree$tip.label %in% labels_to_color], tip = which(tree$tip.label %in% labels_to_color), col = "red", frame = "none", cex = 0.5)
 dev.off()
+
+cat("Tree with colored labels saved to", output_pdf)
