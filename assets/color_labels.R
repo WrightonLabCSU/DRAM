47bd0732876baf4e641a0f1d2b7e850b28b9de48
Reed Woyda <reedwoyda@gmail.com>
2024-05-15 12:53:42 -0600

    Reworking dmso tree package

--- assets/color_labels.R
+++ assets/color_labels.R
@@ -1,39 +1,36 @@
 library(ape)
 
-# Define the input files
-newick_file <- commandArgs(trailingOnly = TRUE)[1]
-labels_file <- commandArgs(trailingOnly = TRUE)[2]
-output_pdf <- commandArgs(trailingOnly = TRUE)[3]
+args <- commandArgs(trailingOnly = TRUE)
+newick_file <- args[1]
+labels_file <- args[2]
+output_pdf <- args[3]
 
-# Read the Newick tree
 tree <- read.tree(newick_file)
-
-# Read the labels to be colored
 labels_to_color_raw <- readLines(labels_file)
 
-# Clean and prepare the labels for matching
-labels_to_color <- gsub("\t", "_", labels_to_color_raw)
+labels_to_color <- gsub("\t", " ", labels_to_color_raw)
+labels_to_color <- trimws(labels_to_color)
 
-# Create a vector for label colors, default to black
-label_colors <- rep("black", length(tree$tip.label))
+tip_colors <- rep("black", length(tree$tip.label))
+tip_labels <- tree$tip.label
 
-# Color the specified labels in red
-label_colors[tree$tip.label %in% labels_to_color] <- "red"
-
-# Plot the tree and extract coordinates
-pdf(output_pdf, width = 10, height = 10)
-plot(tree, type = "unrooted", show.tip.label = FALSE)
-last_plot <- get("last_plot.phylo", envir = .PlotPhyloEnv)
-
-# Add labels with colors and angles
-for (i in 1:length(tree$tip.label)) {
-  label <- tree$tip.label[i]
-  color <- label_colors[i]
-  angle <- last_plot$theta[i]
-  if (!is.na(angle) && angle > 90 && angle < 270) {
-    angle <- angle + 180
+for (i in seq_along(tree$tip.label)) {
+  for (label in labels_to_color) {
+    if (grepl(label, tree$tip.label[i], fixed = TRUE)) {
+      tip_colors[i] <- "red"
+      tip_labels[i] <- tree$tip.label[i]
+      break
+    }
   }
-  text(last_plot$xx[i], last_plot$yy[i], labels = label, pos = 4, cex = 0.6, col = color, srt = angle)
+}
+
+pdf(output_pdf, width = 16, height = 16)
+plot(tree, type = "unrooted", show.tip.label = FALSE)
+
+offset <- 0.02
+cex <- 0.5
+for (i in seq_along(tree$tip.label)) {
+  tiplabels(tip_labels[i], adj = c(0.5, 0.5), frame = "none", col = tip_colors[i], cex = cex, offset = offset)
 }
 
 dev.off()
